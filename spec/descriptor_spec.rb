022bafba129e920471cf1e58620d34bc5cc1051c
Francesco Lazzarino <flazzarino@gmail.com>
2009-08-18 10:15:08 -0400

    spec taken from TODOs in manny's descriptor

---
+++ spec/descriptor_spec.rb
@@ -0,0 +1,17 @@
+describe "aip descriptor" do
+
+  it "should have globally unique identifiers (across the FDA) for events agents and objects"
+  it "We need to add representations, next iteration"
+  
+  describe "premis containers" do
+    it "should reside in its own mets container"
+    it "should be part of the premis namespace"
+  end
+    
+  it "should only have top level validation events, checksum check and failure events only"
+  it "should only have external provenance events if it is found"
+  it "should only have representation retrieval if it is found"
+  it "should only have eventOutcomeDetail if there are anomalies to report"
+  it "should not have an action plan event"
+  
+end
