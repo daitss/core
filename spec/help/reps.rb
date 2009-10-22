b0e8491751618a1a38b24a3daa4bfc9a59a2172a
Francesco Lazzarino <flazzarino@gmail.com>
2009-10-22 07:58:33 -0400

    whitespace ...

--- spec/help/reps.rb
+++ spec/help/reps.rb
@@ -6,7 +6,6 @@ Spec::Matchers.define :have_r0_representation do
   match do |descriptor|
     descriptor_doc = XML::Document.file descriptor
     descriptor_doc.find_first "//mets:techMD/mets:mdWrap[@LABEL='R0']/mets:xmlData/premis:object[@xsi:type='representation']", NS_MAP
-        
   end
 
 end
