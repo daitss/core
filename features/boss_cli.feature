c199c2d49cf7fb605dcb41cfda3164567c4ced9f
Francesco Lazzarino <flazzarino@gmail.com>
2009-11-06 15:24:02 -0500

    starting and stopping single and all packages

--- features/boss_cli.feature
+++ features/boss_cli.feature
@@ -1,9 +1,30 @@
 Feature: Start submissions
   In order ingest submissions
   As an operator
-  I want to start all sips in the workspace that need ingesting
+  I want to start and stop ingests in the workspace
 
   Scenario: start all packages
-    Given a workspace with many sips
-    When I type boss start
-    Then They should show up in the list
+    Given a workspace with many aips
+    When I type boss start for all packages
+    And I type boss list
+    Then they should show up in the list
+    
+  Scenario: stop all packages
+    Given a workspace with many aips
+    When I type boss start for all packages
+    And I type boss stop for all packages
+    And I type boss list
+    Then they should not show up in the list
+
+  Scenario: start a single package
+    Given a workspace with many aips
+    When I type boss start for a single package
+    And I type boss list
+    Then it should show up in the list
+
+  Scenario: stop a single package
+    Given a workspace with many aips
+    When I type boss start for a single package
+    And I type boss stop for that single package
+    And I type boss list
+    Then it should not show up in the list
--- features/step_definitions/boss_steps.rb
+++ features/step_definitions/boss_steps.rb
@@ -1,23 +1,43 @@
-Given /^a workspace with many sips$/ do
+Given /^a workspace with many aips$/ do
   sip = test_sip_by_name 'ateam'
+  2.times { bin "submit #{sip}" }
+  @aips = Dir["#{ENV['DAITSS_WORKSPACE']}/*"]
+  @aips.should have_exactly(2).items
+end
+
+When /^I type boss (start|stop) for (all|a single|that single) packages?$/ do |command, cardinality|
   
-  2.times do
-    `ruby -Ilib bin/submit #{sip}`
-    $?.should == 0
+  aip = case cardinality
+        when "all" then ""
+        when "a single", "that" then File.basename @aips.first
+        end
+  
+  bin "boss #{command} #{aip}"
+end
+
+When /^I type boss list$/ do
+  list_output = bin "boss list"
+  @list = list_output.lines.map { |line| line.chomp.split.first }
+end
+
+Then /^(they|it) (should|should not) show up in the list$/ do |cardinality, condition|
+
+  subject = case cardinality
+            when "they" then @aips
+            when "it" then [@aips.first]
+            end
+
+  case condition
+  when "should"
+    @list.should include(*subject)
+  when "should not"
+    @list.should_not include(*subject)
   end
-  
-  @sips = Dir["#{ENV['DAITSS_WORKSPACE']}/*"]
-  @sips.should have_exactly(2).items
+
 end
 
-When /^I type boss start$/ do
-  `ruby -Ilib bin/boss start`
-  #$?.value.should == 0
-end
-
-Then /^They should show up in the list$/ do
-  output = `ruby -Ilib bin/boss list`
+def bin command
+  output = %x{ruby -Ilib bin/#{command}}
   $?.should == 0
-  lines = output.lines.map { |line| line.chomp }
-  lines.should == @sips
+  output
 end
