c7007c88b33949dbc5618ade784108237011500c
Francesco Lazzarino <flazzarino@gmail.com>
2009-11-11 16:16:12 -0500

    moving to steps

--- features/steps/boss_steps.rb
+++ features/steps/boss_steps.rb
@@ -16,14 +16,17 @@ end
 
 When /^I (type|murmur) "([^\"]*)"$/ do |action, command|
   
-   if action == 'type'
-     if command =~ /^boss start/
-       @last_output = bin_nw command
-     else
-       @last_output = bin command
-     end
-     
-   end
+  if action == 'type'
+
+    case command
+    when /^boss start/, /^boss stop/
+      bin_nw command
+    else
+      @last_output = bin command
+    end
+
+  end
+
 end
 
 Then /^the list should have (\d+) aips?$/ do |size|
@@ -48,6 +51,40 @@ end
 
 Then /^it should return an exit status of 2$/ do
   $?.exitstatus.should == 2
+end
+
+Given /^the following packages with states:$/ do |table|  
+  table.rows.each { Given "I submit a package" }
+  
+  table.rows.each do |aip, state|
+    
+    case state
+    when "pending"
+      # do nothing
+      
+    when "ingesting"
+      bin_nw "boss start #{aip}"
+      $?.exitstatus.should == 0
+      
+    when "REJECT", "SNAFU", "STOP"
+      tag_file = File.join(ENV['DAITSS_WORKSPACE'], aip, state)
+      FileUtils.touch tag_file
+      
+    end
+    
+  end
+  
+  @expected_state_table = table
+end
+
+Then /^I should see the packages with the expected states$/ do
+  
+  actual_state_table = @last_output.lines.inject([["package", "state"]]) do |acc, line|
+                         acc << line.chomp.split
+                         acc
+                       end
+                       
+  @expected_state_table.diff! actual_state_table
 end
 
 
