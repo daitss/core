1966044358452998bc5c64903d9a2e9885dbf67b
Manny Rodriguez <manny@bianchi.fcla.edu>
2009-11-19 14:18:07 -0500

    datamapper class to store submission history

---
+++ lib/submission_history.rb
@@ -0,0 +1,15 @@
+require 'dm-core'
+require 'dm-types'
+
+DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/data/submissions.db")
+
+class Submission
+  include DataMapper::Resource
+
+   property :package_name, String, :nullable => false
+   property :ieid, Serial
+   property :submission_checksum, String, :nullable => false
+   property :timestamp, DateTime, :nullable => false
+   property :submitter_ip, IPAddress, :nullable => false
+end
+
