75224d69eeb7f09a35a643ff918f7e1f157b7c32
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-09 22:21:39 -0500

    moved stron stuff

--- lib/aip/record.rb
+++ lib/aip/record.rb
@@ -1,8 +1,32 @@
 require "dm-core"
 
-class AipRecord
+require 'libxml'
+require 'schematron'
+
+include LibXML
+
+# authoritative aip record
+class Aip
   include DataMapper::Resource
   property :name, String, :key => true
-  property :xml, Text
-  property :needs_work, Boolean
+  property :xml, Text, :nullable => false
+  property :url, String, :nullable => false
+  property :sha1, String, :length => 40, :format => %r([a-f0-9]{40}), :nullable => false
+  property :size, Integer, :min => 1, :nullable => false
+  property :needs_work, Boolean, :nullable => false
+  
+  validates_with_block do
+    doc = XML::Document.string xml
+
+    # validate against schematron
+    results = Aip::SCHEMATRON.validate doc
+
+    unless results.empty?
+      [false, "descriptor fails daitss aip schematron validation (#{results.size} errors)"]
+    else
+      true
+    end
+
+  end
+
 end
