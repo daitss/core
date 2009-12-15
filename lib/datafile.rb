ead8d4862c48cfca4afb58ed4ac6d3857140ba30
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-15 18:44:42 -0500

    uri support for datafile

--- lib/datafile.rb
+++ lib/datafile.rb
@@ -1,17 +1,37 @@
+require 'forwardable'
 require 'fshash'
 require 'fileutils'
+require 'wip'
 
 class DataFile
-  include Transform
 
-  attr_reader :wip, :path, :name, :metadata
+  extend Forwardable
 
-  def initialize wip, name
+  attr_reader :id, :uri, :wip, :metadata, :datapath 
+
+  METADATA_DIR = 'metadata'
+  DATA_FILE = 'data'
+
+  def initialize wip, id
+    @id = id
     @wip = wip
-    @name = name
-    @metadata = FsHash.new File.join(@wip.path, 'file-md', name)
-    @path = File.join @wip.path, 'files', name
-    FileUtils::mkdir_p @path
+    @uri = URI.join(@wip.uri, @id).to_s
+    @dir = File.join @wip.path, Wip::FILES_DIR, @id
+    @metadata = FsHash.new File.join(@dir, METADATA_DIR)
+    @datapath = File.join @dir, DATA_FILE
+    FileUtils::touch @datapath
   end
 
+  def_delegators :@metadata, :[]=, :[], :has_key?, :delete
+
+  def open mode="r"
+
+    if block_given?
+      Kernel::open(@datapath, mode) { |io| yield io }
+    else
+      Kernel::open @datapath, mode
+    end
+
+  end
+  
 end
