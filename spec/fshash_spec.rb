0357a26c97332e8998010e0f94f80430d94f143f
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-15 16:37:54 -0500

    all functions of fshash specced

--- spec/fshash_spec.rb
+++ spec/fshash_spec.rb
@@ -1,30 +1,40 @@
-require 'tempfile'
+require 'spec_helper'
 require 'fshash'
 
 describe FsHash do
 
   subject do
-    t = Tempfile.new FsHash.name
-    path = t.path
-    t.close!
-    FsHash.new path
-  end
-
-  after :all do
-    subject.path
+    FsHash.new $sandbox
   end
 
   it "should persist data between objects" do
     other = FsHash.new subject.path
 
+    # has_key
+    subject.should_not have_key('foo')
+
+    # write
     subject['foo'] = 'bar'
     other['foo'].should == 'bar'
+    subject.should have_key('foo')
     
+    # overwrite
     subject['foo'] = 'baz'
     other['foo'].should == 'baz'
 
-    other['foo'].should == 'bax'
-    subject['foo'].should = 'bax'
+    # overwrite backwards
+    other['foo'] = 'bax'
+    subject['foo'].should == 'bax'
+
+    # empty
+    other['foo'] = nil
+    subject['foo'].should == ""
+    subject.should have_key('foo')
+
+    # delete
+    other.delete 'foo'
+    subject.should_not have_key('foo')
+    subject['foo'].should be_nil
   end
 
 end
