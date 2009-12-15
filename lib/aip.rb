f07a7fe804c2746bf5c1e566415fd655652d4520
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-15 18:48:21 -0500

    simple aip spec

--- lib/aip.rb
+++ lib/aip.rb
@@ -7,6 +7,7 @@ include LibXML
 
 # authoritative aip record
 class Aip
+
   include DataMapper::Resource
   property :name, String, :key => true
   property :xml, Text, :nullable => false
@@ -25,6 +26,38 @@ class Aip
       [false, "descriptor fails daitss aip schematron validation (#{results.size} errors)"]
     else
       true
+    end
+
+    # make sure its stored properly
+    u = URI.parse url
+    req = Net::HTTP::Head.new u.path
+    res = Net::HTTP.start(u.host, copy_url.port) { |http| http.request(req) }
+
+    case res
+    when Net::HTTPSuccess
+    when HTTPNotFound
+      [false, "aip is not stored: #{res.code} #{res.msg}: #{res.body}"]
+    else
+      raise "cannot query aip copy: #{res.code} #{res.msg}: #{res.body}"
+    end
+
+    [false, "fixity is wrong: #{sha1} (record) != #{res['Content-SHA1']} (silo)"] unless res['Content-SHA1'] == sha1
+    [false, "size is wrong: #{size} (record) != #{res['Content-Length']} (silo)"] unless res['Content-Length'] == size
+  end
+
+  def tarball= t
+    size = t.size
+    sha1 = Digest::SHA1.hexdigest t
+
+    u = URI.parse url
+    req = Net::HTTP::Put.new u.path
+    req.body = t
+    res = Net::HTTP.start(u.host, copy_url.port) { |http| http.request(req) }
+
+    case res
+    when Net::HTTPSuccess
+    else
+      raise "cannot store aip: #{res.code} #{res.msg}: #{res.body}"
     end
 
   end
---
+++ spec/aip_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Aip do
+  it "should require a tarball to save"
+  it "should require a url to save"
+  it "should validate against the schematron"
+  it "should validate against the storage"
+  it "should commit it to the database"
+end
+
