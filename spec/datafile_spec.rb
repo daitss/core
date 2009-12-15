5bcb01ccff55e85fb02fde08c6cc4b630adb6f0c
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-15 18:44:42 -0500

    uri support for datafile

--- spec/datafile_spec.rb
+++ spec/datafile_spec.rb
@@ -1,8 +1,26 @@
+require 'spec_helper'
 require 'datafile'
 
 # Proto AIP: Work In Progress
 describe DataFile do
-  it "should persist on the file system"
-  it "should let one create new files"
-  it "should let one add new metadata"
+
+  subject do
+    wip = Wip.new File.join($sandbox, UG.generate), 'bogus:/'
+    wip.new_datafile
+  end
+  
+  it "should let one add new metadata" do
+    subject['sip-path'] = 'foo/bar/baz'
+    subject['sip-path'].should == 'foo/bar/baz'
+  end
+  
+  it "should let one read and write the data" do
+    subject.open("w") { |io| io.write "foo" }
+    subject.open { |io| io.read }.should == "foo"
+  end
+
+  it "should have a uri" do
+    subject.uri.should == URI.join(subject.wip.uri, subject.id).to_s
+  end
+  
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,38 @@
+require 'uuid'
+require 'datamapper'
+require 'fileutils'
+require 'tempfile'
+
+UG = UUID.new
+
+def new_sandbox
+  tf = Tempfile.new 'sandbox'
+  path = tf.path
+  tf.close!
+
+  if block_given?
+    FileUtils::mkdir_p path
+    yield path
+    FileUtils::rm_rf path
+  else
+    path
+  end
+
+end
+
+Spec::Runner.configure do |config|
+
+  config.before :all do
+    $sandbox = new_sandbox
+    FileUtils::mkdir_p $sandbox
+
+    # An in-memory Sqlite3 connection
+    DataMapper.setup :default, 'sqlite3::memory:'
+    DataMapper.auto_migrate!
+  end
+
+  config.after :all do
+    FileUtils::rm_rf $sandbox
+  end
+  
+end
