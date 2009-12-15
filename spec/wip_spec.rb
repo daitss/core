af7854bf33d647f597cf62013a4c4fcc36340ead
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-15 18:44:00 -0500

    uri

--- spec/wip_spec.rb
+++ spec/wip_spec.rb
@@ -1,9 +1,32 @@
+require 'spec_helper'
 require 'wip'
+require 'uuid'
+gen = UUID.new
 
 # Proto AIP: Work In Progress
 describe Wip do
-  it "should persist on the file system"
-  it "should let addition of new files"
-  it "should let addition of new metadata"
-  it "should let new tags be set"
+
+  subject { Wip.new File.join($sandbox, gen.generate), "bogus:/" }
+
+  it "should let addition of new files" do
+    df = subject.new_datafile 
+    df['sip-path'] = 'foo/bar.png'
+  end
+
+  it "should let addition of new metadata" do
+    subject['submit-event'] = "submitted at #{Time.now}"
+    
+    wip_prime = Wip.new File.join($sandbox, subject.id), "bogus:/"
+    subject['submit-event'].should == wip_prime['submit-event']
+  end
+
+  it "should let new tags be set" do
+    subject.tags['FOO'] = '100'
+    subject.tags['FOO'].should == '100'
+  end
+
+  it "should have a uri" do
+   subject.uri.should == URI.join("bogus:/", subject.id).to_s 
+  end
+
 end
