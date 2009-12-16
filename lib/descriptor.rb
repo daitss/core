fc559d9237f996c160a4a34fd1a8611f1b4ca36d
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-15 21:08:40 -0500

    new algorith and aip

--- lib/descriptor.rb
+++ lib/descriptor.rb
@@ -2,13 +2,40 @@
 module Descriptor
 
   def descriptor
-    template = template_by_name 'aip_descriptor'
-    template.result binding
+    template_by_name('aip/descriptor').result binding
   end
-  
-  def md_section id, doc
-    template = template_by_name 'md_section'
-    template.result binding
+
+  private
+
+  def tag_start name, attributes={}
+    attr_string = attributes.map { |(name, value)| "#{name.id2name}=\"#{value}\"" }.join ' ' 
+    "<#{name} #{attr_string}>"
+  end
+
+  def tag_end name
+    "</#{name} #{attr_string}>"
+  end
+
+  def tag_single name, attributes={}
+    attr_string = attributes.map { |(name, value)| "#{name.id2name}=\"#{value}\"" }.join ' ' 
+    "<#{name} #{attr_string}/>"
+  end
+
+  def file_element f, id
+    template_by_name('aip/file_element').result binding
+  end
+
+  def intellectual_entity_object
+    template_by_name('aip/intellectual_entity_object').result binding
+  end
+
+  def representation_object rep
+    template_by_name('aip/representation_object').result binding
+  end
+
+  def md_section doc, options={}
+    options[:md_type] ||= 'PREMIS'
+    template_by_name('aip/md_section').result binding
   end
 
 end
