85a5327bc5065f373228c5728a580f3a35a607ce
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-21 11:26:27 -0500

    wip-based validation protocol

--- lib/service/validate.rb
+++ lib/service/validate.rb
@@ -12,61 +12,56 @@ module Service
   module Validate
 
     def validated?
-      metadata.has_key? 'validate'
+      metadata.has_key? 'validate-event'
     end
 
     def validate!
-      url = URI.parse "#{CONFIG['validation']}?location=#{CGI::escape @url.to_s}"
-      req = Net::HTTP::Get.new u.request_uri
-
-      res = Net::HTTP.start(u.host, u.port) do |http|
-        http.read_timeout = 10 * 60
-        http.request(req)
-      end
-
-      doc = case res
-            when Net::HTTPSuccess then
-              XML::Document.string res.body
-            else
-              raise "cannot validate: #{response.code} #{response.msg}: #{response.body}"
-            end
-
-      # TODO fix the ids in the xml
-
-      # reject if needed
-      raise Reject, reject_reasons(val_doc) if need_to_reject? val_doc
-      strip_cruft! doc
-      metadata['validate'] = doc.to_s
-
+      doc = XML::Document.string ask_validation
+      rr = reject_reasons doc
+      raise Reject, rr unless rr.empty?
+      metadata['validate-event'] = validate_event doc
+      metadata['validate-agent'] = validate_agent doc
     end
 
     private
 
-    GLOBAL_VALIDATION_EVENT_TYPE = 'SIP passed all validation checks' 
+    def ask_validation
+      url = URI.parse "#{CONFIG['validation-url']}?location=#{CGI::escape URI.join('file:/', File.expand_path(path)).to_s }"
+      req = Net::HTTP::Get.new url.request_uri
 
-    def reject_reasons doc
-      failed_event_xpath = "//P:event[P:eventOutcomeInformation/P:eventOutcome['failure']]"
+      res = Net::HTTP.start(url.host, url.port) do |http|
+        http.read_timeout = 10 * 60
+        http.request(req)
+      end
 
-      reject_reasons = doc.find(failed_event_xpath, NS_PREFIX).map do |fe|
-        {
-          :type => fe.find_first('P:eventType', NS_PREFIX).content.strip,
-          :time => Time.parse(fe.find_first('P:eventDateTime', NS_PREFIX).content.strip),
-          :message => fe.find_first('P:eventOutcomeInformation/P:eventOutcome', NS_PREFIX).content.strip
-        }
+      case res
+      when Net::HTTPSuccess then res.body
+      else res.error!
       end
 
     end
 
-    def need_to_reject? doc
-      policy_event = doc.find_first("//P:event[P:eventType='#{GLOBAL_VALIDATION_EVENT_TYPE}']", NS_PREFIX)
-      raise "cannot determine validation of package" unless policy_event
-      policy_event.find_first("P:eventOutcomeInformation[P:eventOutcome='failure']", NS_PREFIX)
+    def reject_reasons doc
+      xpath = "//P:event[P:eventOutcomeInformation/P:eventOutcome ='failure' ]"
+
+      doc.find(xpath, NS_PREFIX).map do |e|
+        { :type => e.find_first('P:eventType', NS_PREFIX).content.strip,
+          :time => Time.parse(e.find_first('P:eventDateTime', NS_PREFIX).content.strip),
+          :message => e.find_first('P:eventOutcomeInformation/P:eventOutcome', NS_PREFIX).content.strip }
+      end
+
     end
 
-    def strip_cruft! doc
-      xpath = "/P:premis/P:event[P:eventType != '#{GLOBAL_VALIDATION_EVENT_TYPE}']"
-      doc.find(xpath, NS_PREFIX).each { |node| node.remove! }
+    def validate_event doc
+      xpath = "//P:event[P:eventType = 'SIP passed all validation checks']"
+      doc.find_first(xpath, NS_PREFIX).to_s
+    end
+
+    def validate_agent doc
+      xpath = "/P:agent[P:agentName = 'Validation Service']"
+      doc.find_first(xpath, NS_PREFIX).to_s
     end
 
   end
+
 end
