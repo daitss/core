4d79144d707e169d62ca5b0d581e10d81c61c5ed
Francesco Lazzarino <flazzarino@gmail.com>
2009-12-21 11:31:47 -0500

    simpler transformation model

--- lib/service/transform.rb
+++ lib/service/transform.rb
@@ -5,25 +5,65 @@ module Service
 
   module Transform
 
-    def migrated_src
-      transform_src 'migration'
+    # Returns a new datafile provided there is a preservation policy to migrate
+    def migrate
+
+      preserve(CONFIG['migrate-uri']) do |df, tr_url|
+        event_id = URI.join(df.uri, 'event', 'migrate').to_s
+        df['migrate-event'] = event :id => event_id, :type => 'migrate'
+        df['migrate-agent'] = agent :id => tr_url, :name => 'transformation service', :type => 'software'
+        df.describe! :transform_src => uri, :transform_event => event_id
+      end
+
     end
 
-    def normalized_src
-      transform_src 'normalization'
+    # Returns a new datafile provided there is a preservation policy to normalize
+    def normalize
+
+      preserve(CONFIG['normalize-uri']) do |df, tr_url|
+        event_id = URI.join(df.uri, 'event', 'normalize').to_s
+        df['normalize-event'] = event :id => event_id, :type => 'normalize'
+        df['normalize-agent'] = agent :id => tr_url, :name => 'transformation service', :type => 'software'
+        df.describe! :transform_src => uri, :transform_event => event_id
+      end
+
     end
 
     private
 
-    def transform_src type
+    def preserve ap_url, &blk
+      actionplan(ap_url) { |tr_url| transform tr_url, &blk }
+    end
 
-      file_id = metadata.find do |key, doc| 
-        event = doc.find_first "//P:event[P:eventType = '#{type}']", NS_PREFIX
-        related_object_id = event.find_first "P:relatedObjectIdentifier/P:relatedObjectIdentifierValue". NS_PREFIX
-        related_object_id.content
+    def actionplan url
+
+      Net::HTTP.post_form(URI.parse(ap_url), 'description' => metadata['describe-object']) do |res|
+
+        case res
+        when Net::HTTPRedirection then yield res['location']
+        when Net::HTTPNotFound then nil
+        else res.error!
+        end
+
       end
 
-      wip.files.find { |f| f.id == file_id }
+    end
+
+    def transform url
+
+      Net::HTTP.post_form(URI.parse(tr_url), 'data' => self.open { |io| io.read } ) do |res|
+
+        case res
+        when Net:HTTPSuccess
+          df = wip.new_datafile
+          df.open(:w) { |io| io.write res.body }
+          yield df, transformation_url
+          df
+        else res.error!
+        end
+
+      end
+
     end
 
   end
