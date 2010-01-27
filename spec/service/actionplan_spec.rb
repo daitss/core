109c14daaae048666e700b19d1895fb7fbfef14e
Francesco Lazzarino <flazzarino@gmail.com>
2010-01-27 16:17:54 -0500

    actionplan interaction specified

--- spec/service/actionplan_spec.rb
+++ spec/service/actionplan_spec.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 require 'xmlns'
+require 'service/describe'
 require 'service/actionplan'
 
 describe 'action planning a datafile' do
 
   subject do
     wip = submit_sip 'mimi'
-    wip.datafiles.find { |df| df['sip-path'] =~ %r{\.pdf$} }
+    df = wip.datafiles.find { |df| df['sip-path'] == 'mimi.pdf' }
+    df.describe!
+    df
   end
 
   it "should return nil if there is no migration" do
     subject.migration.should be_nil
+  end
+
+  it "should redirect if there is a transformation" do
+    subject.normalization.should == 'http://localhost:7000/transformation/pdf_norm'
   end
 
 end
