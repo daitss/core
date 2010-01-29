acbdfa480a9cea51cf134cd4e3b00a55a0e1cdd9
Francesco Lazzarino <flazzarino@gmail.com>
2010-01-29 16:02:48 -0500

    clean namespace on open

--- lib/template.rb
+++ lib/template.rb
@@ -3,6 +3,6 @@ require 'erb'
 include ERB::Util
 
 def template_by_name name
-  file = File.join(File.dirname(__FILE__), '..', 'templates', "#{name}.erb")
-  open(file) { |io| ERB.new io.read }
+  file = File::expand_path File::join(File.dirname(__FILE__), '..', 'templates', "#{name}.erb")
+  Kernel::open(file) { |io| ERB.new io.read }
 end
