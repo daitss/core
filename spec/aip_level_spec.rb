7747ad17fb0d502b6446e4b5217a23fb45e21825
Francesco Lazzarino <flazzarino@gmail.com>
2010-01-29 16:55:44 -0500

    removing cruft

--- spec/aip_level_spec.rb
+++
@@ -1,6 +0,0 @@
-  it "should continue an incomplete ingest (pick up where it left off)" do
-  it "should collect incoming representations" do
-  it "should collect incoming tipr provenance" do
-  it "should collect incoming provenance" do
-  it "should raise a rejection error if validation fails" do
-
