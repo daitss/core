cc140d8d64b1ed306b4da9ff4a8f08f07d99d1fe
Francesco Lazzarino <flazzarino@gmail.com>
2010-01-29 15:36:25 -0500

    description allows migration specific descriptions

--- lib/service/describe.rb
+++ lib/service/describe.rb
@@ -1,10 +1,11 @@
 require 'datafile'
+require 'template/premis'
 require 'net/http'
 require 'cgi'
 
 class DataFile
 
-  def describe! 
+  def describe! options={}
     doc = ask_description_service(:location => "file:#{File.expand_path datapath }",
                                   :uri => uri, 
                                   :originalName => metadata['sip-path'])
@@ -12,7 +13,32 @@ class DataFile
     metadata['describe-event'] = element_doc_as_str doc, "//P:event"
     metadata['describe-agent'] = element_doc_as_str doc, "//P:agent" 
     metadata['describe-bitstream-objects'] = element_doc_as_str doc, "//P:object[@xsi:type='bitstream']"
-    @wip.tags["describe-#{id}"] = Time.now.xmlschema
+
+    if options[:derivation_source] and options[:derivation_method]
+      event_uri = "#{options[:derivation_source]}/event/#{options[:derivation_method]}"
+      metadata["#{options[:derivation_method]}-event"] = event :id => event_uri, :type => options[:derivation_method]
+
+      rel_doc = XML::Document::string relationship(:type => 'derivation',
+                                                   :sub_type => 'has source',
+                                                   :related_objects => [ options[:derivation_source] ],
+                                                   :related_events => [event_uri])
+      #
+      # update the description
+      doc = XML::Document::string metadata['describe-file-object']
+      rel = doc.import rel_doc.root
+      object = doc.find_first "/P:object[@xsi:type='file']", NS_PREFIX
+      insertion_point = object.find_first "P:linkingEventIdentifier | P:linkingIntellectualEntityIdentifier | P:linkingRightsStatementIdentifier", NS_PREFIX
+
+      if insertion_point
+        insertion_point.prev = rel
+      else
+        object << rel
+      end
+
+      metadata['describe-file-object'] = doc.to_s
+
+    end
+
   end
 
   private
@@ -20,7 +46,7 @@ class DataFile
   def ask_description_service query={}
     query_str = query.map { |key, value| "#{key.id2name}=#{CGI::escape value.to_s}" }.join '&'
     url = URI.parse "#{CONFIG['description-url']}?#{query_str}"
-    res = Net::HTTP.get_response url
+      res = Net::HTTP.get_response url
 
     case res
     when Net::HTTPSuccess then XML::Document.string res.body
