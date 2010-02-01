9e86e0fd93a2baec431ac0cb75c930a59d8b5628
Francesco Lazzarino <flazzarino@gmail.com>
2010-02-01 16:11:58 -0500

    code to transform a wip into an aip factored

---
+++ spec/aip/insert_spec.rb
@@ -0,0 +1,27 @@
+require 'service/validate'
+require 'spec_helper'
+require 'wip/preserve'
+require 'db/aip/wip'
+
+describe Aip do
+
+  it "should create a new instance based on a WIP" do
+    wip = submit_sip 'mimi'
+    wip.validate!
+    wip.preserve!
+    wip['aip-descriptor'] = wip.descriptor
+
+    spec = {
+      :id => "#{wip.uri}/event/ingest", 
+      :type => 'ingest', 
+      :outcome => 'success', 
+      :linking_objects => [ wip.uri ]
+    }
+
+    wip['ingest-event'] = event spec
+
+    Aip::new_from_wip wip
+    lambda { Aip.get! wip.id }.should_not raise_error(DataMapper::ObjectNotFoundError)
+  end
+
+end
