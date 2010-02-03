b939b4ce1b7e1b2d252d9e480a56f4999d6b58b6
Francesco Lazzarino <flazzarino@gmail.com>
2010-02-03 18:33:05 -0500

    test pacakge cruft removal

--- spec/help/test_package.rb
+++ spec/help/test_package.rb
@@ -3,37 +3,9 @@ require "wip/create"
 require "template/premis"
 require "uuid"
 
-TEST_PACKAGE_DIR = File.join File.dirname(__FILE__), '..', '..', 'test-packages'
-TEST_SIPS_DIR = File.join TEST_PACKAGE_DIR, 'sips'
+TEST_SIPS_DIR = File.join File.dirname(__FILE__), '..', 'sips'
 URI_PREFIX = 'test:/'
 UG = UUID.new
-
-def test_sip_by_name name
-  p = File.join test_package_dir, 'sips', name
-  File.expand_path p
-end
-
-def test_aip_by_name name
-  p = File.join test_package_dir, 'aips', name
-  File.expand_path p
-end
-
-def aip_instance_path name
-  prototype = test_aip_by_name name
-  FileUtils::cp_r prototype, $sandbox
-  path = File.join $sandbox, name
-  path
-end
-
-def aip_instance name
-  Aip.new "file:#{aip_instance_path name}"
-end
-
-def aip_instance_from_sip name
-  sip = test_sip_by_name name
-  path = File.join $sandbox, 'aip'
-  Wip.make_from_sip path, sip
-end
 
 def submit_sip name
   sip = Sip.new File.join(TEST_SIPS_DIR, name)
