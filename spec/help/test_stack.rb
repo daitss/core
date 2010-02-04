83b6a67c0be8205746d9753b2f69ac07b8588b4a
Francesco Lazzarino <flazzarino@gmail.com>
2010-02-04 17:44:11 -0500

    disseminate spec (not passing yet)

--- spec/help/test_stack.rb
+++ spec/help/test_stack.rb
@@ -3,6 +3,7 @@ require "config"
 # configuration for the test stack
 TEST_STACK_CONFIG_FILE = File.join File.dirname(__FILE__), '..', 'config', 'teststack.yml'
 CONFIG::load TEST_STACK_CONFIG_FILE
+STATUS_ECHO_URL = 'http://localhost:7000/statusecho'
 
 # place to put stored copies
 $silo_sandbox='/tmp/silo_sandbox'
--- spec/wip/disseminate_spec.rb
+++ spec/wip/disseminate_spec.rb
@@ -1,3 +1,49 @@
-# TODO import old package level provenance
-# TODO make sure obsolete datafiles have premis objects
-# TODO import old data file level provenance for each data file even obsolete ones
+require 'spec_helper'
+require 'wip'
+require 'wip/disseminate'
+require 'datamapper'
+
+describe Wip do
+
+  describe "that cannot disseminate" do
+
+    it "should fail if an aip does not exist for the wip" do
+      wip = submit_sip 'mimi'
+      lambda { wip.disseminate }.should raise_error(DataMapper::ObjectNotFoundError)
+    end
+
+    it "should snafu if there is anything wrong with dissemination" do
+      wip = ingest_sip 'mimi'
+      real_description_url = CONFIG['description-url']
+      CONFIG['description-url'] = "#{STATUS_ECHO_URL}/500/foo"
+      wip.disseminate
+      wip.should be_snafu
+      CONFIG['description-url'] = real_description_url
+    end
+
+    describe "that can disseminate" do
+
+      subject do 
+        wip = ingest_sip 'mimi'
+        wip.disseminate
+        wip
+      end
+
+      it "should update the aip it came from" do
+         aip = Aip.get subject.id 
+         aip.should_not be_nil
+         doc = XML::Document.string aip.xml
+         disseminate_event = doc.find_first "P:event[P:eventType = 'disseminate']"
+         disseminate_event.should_not be_nil
+      end
+
+      it "should produce a dip in a disseminate area" do
+          path = File.join CONFIG['disseminate-dir-path'], "#{subject.id}.tar"
+          File.exist?(path).should be_true
+      end
+
+    end
+
+  end
+
+end
