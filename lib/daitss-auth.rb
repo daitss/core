c0da3ad40df4f2e138d02d0b0da327dae5ce1324
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-02-12 17:06:29 -0500

    authentication class stub

---
+++ lib/daitss-auth.rb
@@ -0,0 +1,12 @@
+require 'db/operations_agents'
+
+class Authentication
+
+  # given a id, key pair, return a data structure which contains:
+  # authenticated? yes or no
+  # all known metadata about agent
+  
+  def authenticate agent_identifier, key
+  end
+end
+
