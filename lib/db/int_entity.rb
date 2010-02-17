939653e5578c53ce73adf1a93772c90006a4c414
Carol <cchou@ufl.edu>
2010-02-17 13:58:12 -0500

    build int entity based on the latest aip descriptor, still need work.  pending more finalized aip

--- lib/db/int_entity.rb
+++ lib/db/int_entity.rb
@@ -12,8 +12,21 @@ class Intentity
   # has 0..n, :intentity_events
   has 1..n, :representations
   
-  def fromPremis
-    # TODO: implement
-    attribute_set(:id, "E00000000_000000")
+  def fromPremis premis
+    id = premis.find_first("//p2:intellectualEntity/p2:intellectualEntityIdentifier/p2:intellectualEntityIdentifierValue", NAMESPACES)
+    if id
+      attribute_set(:id, id.content)
+    else
+      #TODO: this is only temporary, should be removed once AIP descriptor building is complete
+      attribute_set(:id, "E00000000_000000")
+    end
+  end
+  
+  def match id
+    matched = false
+    if id && id == @id
+      matched = true
+    end
+    matched
   end
 end
