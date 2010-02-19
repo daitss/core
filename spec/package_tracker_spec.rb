30d38d8201f1d11c15e7e80ca18bdbdc1b27b062
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-02-19 10:22:41 -0500

    implementation and tests for PackageTracker.insert_op_event

--- spec/package_tracker_spec.rb
+++ spec/package_tracker_spec.rb
@@ -1,5 +1,6 @@
 require 'package_tracker'
 require 'helper'
+require 'time'
 require 'pp'
 
 describe PackageTracker do
@@ -15,8 +16,31 @@ describe PackageTracker do
 
     PackageTracker.insert_op_event "operator", "xxxx-xxxx-xxxx-xxxx-xxxx-xxxxx", "WIP Stash", "this string should end up in the notes field"
 
-    event = OperationsEvent.all(:operations_agent => {:identifier => "operator"})   #event = OperationsEvent.all
+    event = OperationsEvent.all.pop
+    agent = event.operations_agent
 
-    pp event
+    event.timestamp.to_s.should == Time.now.iso8601
+    event.event_name == "WIP Stash"
+    event.notes == "this string should end up in the notes field"
+    event.ieid == "xxxx-xxxx-xxxx-xxxx-xxxx-xxxxx"
+
+    agent.identifier.should == "operator"
+  end
+
+  it "should raise exception when adding an event associated with an agent that does not exist" do
+    lambda { PackageTracker.insert_op_event "foovar", "xxxx-xxxx-xxxx-xxxx-xxxx-xxxxx", "WIP Stash", "this string should end up in the notes field" }.should raise_error
+  end
+
+  it "should raise exception when adding an event associated with an IEID that does not exist" do
+    pending "integration to the int entity table"
+  end
+
+  it "should raise exception when attempting to add an event with an empty or null name" do
+    a = add_account
+    add_operator a
+
+    lambda { PackageTracker.insert_op_event "operator", "xxxx-xxxx-xxxx-xxxx-xxxx-xxxxx", "", "this string should end up in the notes field" }.should raise_error
+
+    lambda { PackageTracker.insert_op_event "operator", "xxxx-xxxx-xxxx-xxxx-xxxx-xxxxx", nil, "this string should end up in the notes field" }.should raise_error
   end
 end
