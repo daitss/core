45b23f0b1e89b5ae3b5e0631cf56e55f7a2f3989
Carol <cchou@ufl.edu>
2010-02-24 11:05:18 -0500

    update aip store after successful population

--- lib/AIPPolling.rb
+++ lib/AIPPolling.rb
@@ -7,14 +7,24 @@ class AIPPolling
   repository(:aipstore) do
     @needWorkAIP = Aip.all(:needs_work => true) 
   end
-  
-  repository(:default) do
-    @needWorkAIP.each do |aip|
-      doc = XML::Document.string(aip.xml)
-      doc.save('aip.xml', :indent => true, :encoding => LibXML::XML::Encoding::UTF_8)
-      aipInPremis = AIPInPremis.new
-      aipInPremis.process doc
+
+  @needWorkAIP.each do |aip|
+    begin
+      repository(:default) do
+        puts "processing #{aip.uri}"
+        doc = XML::Document.string(aip.xml)
+        doc.save('aip.xml', :indent => true, :encoding => LibXML::XML::Encoding::UTF_8)
+        aipInPremis = AIPInPremis.new
+        aipInPremis.process doc
+      end
+    rescue 
+      puts "problem populating #{aip.uri}, daitss 2 dataase is not updated!"
+    else #only update aip store after a successful daitss2 db population
+      repository(:aipstore) do    
+      aip.update(:needs_work => false)
     end
   end
+  
+  # sleep 5
 end
 
