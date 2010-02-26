ce43ec0ed881e00eef5a142c8e1994ce4b15545b
Francesco Lazzarino <flazzarino@gmail.com>
2010-02-26 14:24:57 -0500

    steps have durations

--- spec/wip/step_spec.rb
+++ spec/wip/step_spec.rb
@@ -6,26 +6,20 @@ describe Wip do
 
   describe "that takes soft steps" do
 
-    it "should should return a value of the block" do
-      wip = submit_sip 'mimi'
-      value = wip.step('add'){ 2 + 3 }
-      value.should == 5
-    end
-
     it "should make a step tag with the timestamp" do
       wip = submit_sip 'mimi'
       wip.step('add'){ 2 + 3 }
-      wip.step_time('add').should_not be_nil
-      wip.step_time('add').should < Time.now
+      wip.step_end_time('add').should_not be_nil
+      wip.step_end_time('add').should < Time.now
     end
 
     it "should not perform an operation if the step has been performed" do
       wip = submit_sip 'mimi'
       wip.step('add'){ 2 + 3 }
-      time = wip.step_time('add')
+      time = wip.step_end_time('add')
       sleep 1.5
       wip.step('add'){ 2 + 3 }
-      wip.step_time('add').should == time
+      wip.step_end_time('add').should == time
     end
 
   end
@@ -35,10 +29,10 @@ describe Wip do
     it "should perform an operation even if the step has been performed" do
       wip = submit_sip 'mimi'
       wip.step('add'){ 2 + 3 }
-      time = wip.step_time('add')
+      time = wip.step_end_time('add')
       sleep 1.5
       wip.step!('add'){ 2 + 3 }
-      wip.step_time('add').should > time
+      wip.step_end_time('add').should > time
     end
 
   end
