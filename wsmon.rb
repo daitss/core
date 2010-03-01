813360d2326ed22a15093faf5c0c719839b42d1a
Francesco Lazzarino <flazzarino@gmail.com>
2010-03-01 14:11:30 -0500

    workspace checking json representation simpler task oriented posts

--- wsmon.rb
+++ wsmon.rb
@@ -12,6 +12,7 @@ configure do
 
   # workspace
   raise "no workspace" unless ENV['WORKSPACE']
+  raise "#{ENV['WORKSPACE']} is not a directory" unless File.directory? ENV['WORKSPACE']
   WORKSPACE = Workspace.new ENV['WORKSPACE']
 
   # configuration
@@ -83,12 +84,39 @@ get '/stylesheet.css' do
 end
 
 get '/' do
-  haml :index
+
+  if request.accept.include? 'application/json'
+    WORKSPACE.to_json
+  else
+    haml :index
+  end
+
+end
+
+post '/' do
+
+  case params['task']
+  when 'start'
+    startable = WORKSPACE.reject { |w| w.running? || w.done? }
+    startable.each { |wip| wip.start_task }
+    
+  when 'stop'
+    stoppable = WORKSPACE.select { |w| w.running? }
+    stoppable.each { |wip| wip.stop }
+
+  when 'unsnafu'
+    unsnafuable= WORKSPACE.select { |w| w.snafu? }
+    unsnafuable.each { |wip| wip.unsnafu! }
+
+  when nil, '' then error 400, "parameter task is required"
+  else error 400, "unknown command: #{params['task']}"
+  end
+
+  redirect '/'
 end
 
 get '/:id' do |id|
-  not_found "wip #{id} not found" unless WORKSPACE.has_wip? id
-  @wip = WORKSPACE[id]
+  @wip = WORKSPACE[id] or not_found
 
   if request.accept.include? 'application/json'
     @wip.to_json
@@ -99,34 +127,29 @@ get '/:id' do |id|
 end
 
 post '/:id' do |id|
-  not_found "wip #{id} not found" unless WORKSPACE.has_wip? id
-  wip = WORKSPACE[id]
+  wip = WORKSPACE[id] or not_found
 
-  # update snafu status
-  case params['snafu']
-  when 'true'
-    error 400, 'manaul snafu not supported'
-
-  when 'false' 
-    error 400, "cannot unsnafu a package that is not snafu" unless wip.snafu?
-    wip.unsnafu!
-
-  when nil
-  else error 400, "only valid snafu values are true and false"
-  end
-
-  # update process
-  case params['process']
+  case params['task']
   when 'start'
-    error 400, "cannot start a running wip" if wip.running?
+    error 400, 'cannot start a running wip' if wip.running?
     wip.start_task
 
-  when 'stop' 
-    error 400, "cannot stop a stopped wip" unless wip.running?
+  when 'stop'
+    error 400, 'cannot stop an idle wip' unless wip.running?
     wip.stop
 
-  when nil
-  else error 400, "only valid states are start and stop"
+  when 'unsnafu'
+    error 400, 'can only unsnafu a snafu wip' unless wip.snafu?
+    wip.unsnafu!
+
+  when 'stash'
+    error 400, 'parameter path is required' unless params['path']
+    error 400, "#{params['path']} is not a directory" unless File.directory? params['path']
+    FileUtils::mv wip.path, params['path']
+    redirect '/'
+
+  when nil, '' then raise 400, 'parameter task is required'
+  else error 400, "unknown command: #{params['task']}"
   end
 
   redirect wip.id
