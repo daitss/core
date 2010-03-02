880fe687896081cb85d3857971550ac1d6327ab2
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-03-02 09:33:37 -0500

    modified submission client to take username and password on the command line

--- setup-database.rb
+++ setup-database.rb
@@ -7,57 +7,13 @@ require 'db/accounts'
 require 'db/projects'
 require 'db/keys'
 require 'digest/sha1'
+require 'spec/helper.rb'
 
 DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/data/submission_svc_test.db")
-
 DataMapper.auto_migrate!
 
-# add an FDA account
-a = Account.new
-a.attributes = { :name => "FDA",
-                 :code => "FDA" }
-a.save!
+# adds an FDA account, and an Operator with credentials "operator", "operator"
 
-# add an operator user 
-
-o = Operator.new  
-o.attributes = { :description => "operator",
-  :active_start_date => Time.at(0),
-  :active_end_date => Time.now + (86400 * 365),
-  :identifier => "operator",
-  :first_name => "Op",
-  :last_name => "Perator",
-  :email => "operator@ufl.edu",
-  :phone => "666-6666",
-  :address => "FCLA" }
-
-o.account = a
-
-k = AuthenticationKey.new
-k.attributes = { :auth_key => Digest::SHA1.hexdigest("operator") }
-
-o.authentication_key = k
-o.save!
-
-# add a contact lacking permissions to submit
-
-c = Contact.new
-c.attributes = { :description => "contact",
-  :active_start_date => Time.at(0),
-  :active_end_date => Time.now + (86400 * 365),
-  :identifier => "foobar",
-  :first_name => "Foo",
-  :last_name => "Bar",
-  :email => "foobar@ufl.edu",
-  :phone => "555-5555",
-  :address => "123 Toontown",
-  :permissions => [:disseminate, :withdraw, :peek] }
-
-c.account = a
-
-j = AuthenticationKey.new
-j.attributes = { :auth_key => Digest::SHA1.hexdigest("foobar") }
-
-c.authentication_key = j
-c.save!
+a = add_account
+o = add_operator a
 
