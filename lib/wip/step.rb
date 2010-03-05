e4f5cdd9a1b97a97ce3b5ab96bedbee7d0e7cffe
Francesco Lazzarino <flazzarino@gmail.com>
2010-03-04 23:39:56 -0500

    steps are using marks

--- lib/wip/step.rb
+++ lib/wip/step.rb
@@ -1,51 +1,33 @@
 require 'wip'
+require 'mark'
 
 class Wip
 
   def step name
     key = step_key name
 
-    unless tags.has_key? key
-      start_time = Time.now
-      value = yield
-      end_time = Time.now
-      tags[key] = "#{start_time.xmlschema 4} #{end_time.xmlschema 4}"
-      value
+    if tags.has_key? key
+      Mark.parse tags[key]
+    else
+      step!(name){ yield }
     end
 
   end
 
   def step! name
     key = step_key name
-
-    start_time = Time.now
+    m = Mark.new
+    m.start
     value = yield
-    end_time = Time.now
-    tags[key] = "#{start_time.xmlschema 4} #{end_time.xmlschema 4}"
-    value
+    m.finish
+    tags[key] = m.to_s
+    m
   end
 
-  def step_start_time name
-    key = step_key name
-    start_time, end_time = tags[key].split(' ').map { |t| Time.parse t } if tags.has_key? key
-    end_time
-  end
-
-  def step_end_time name
-    key = step_key name
-    start_time, end_time = tags[key].split(' ').map { |t| Time.parse t } if tags.has_key? key
-    end_time
-  end
-
-  def duration name
-    key = step_key name
-    start_time, end_time = tags[key].split(' ').map { |t| Time.parse t } if tags.has_key? key
-    end_time - start_time
-  end
   private 
 
   def step_key name
-    "step-#{name}"
+    "step.#{name}"
   end
 
 end
