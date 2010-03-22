b8fdd532f256e32d65e47ca25eaf2be765b6dc74
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-03-22 15:59:01 -0400

    added a rake task to automigrate database with aip store and ops database for testing

--- tasks/service_stack.rb
+++ tasks/service_stack.rb
@@ -104,6 +104,22 @@ def service_stack
 
 end
 
+namespace :database do
+  desc "populate the database"
+  task :db_migrate do
+    require 'db/accounts'
+    require 'db/aip'
+    require 'db/keys'
+    require 'db/operations_agents'
+    require 'db/operations_events'
+    require 'db/package_location'
+    require 'db/projects'
+
+    DataMapper.setup :default, Daitss::CONFIG['database-url']
+    DataMapper.auto_migrate!
+  end
+end
+
 namespace :services do
 
   desc "fetch the services"
