512aa5f39ce3522151000144956613b81b1e8f24
Francesco Lazzarino <flazzarino@gmail.com>
2010-04-12 11:33:43 -0400

    xml resolution services interaction is done

--- lib/wip/xmlresolve.rb
+++ lib/wip/xmlresolve.rb
@@ -1,11 +1,29 @@
 require 'wip'
+require 'datafile/actionplan'
 
 class Wip
 
-  def xmlresolve
+  def xmlresolve!
+    url = put_collection_resource + '/'
+    resolve_datafiles url
+    tar = get_tarball url
 
-    # PUT to new resource
-    url = URL.parse "#{Daitss::CONFIG['xmlresolution-url']}/ieids/#{id}"
+    metadata['xml-resolution-tarball'] = tar
+    metadata['xml-resolution-event'] = event(:id => "#{uri}/event/xmlresolution",
+                                             :type => 'xml resolution',
+                                             :outcome => 'success',
+                                             :linking_objects => [ uri ],
+                                             :linking_agents => [ Daitss::CONFIG['xmlresolution-url'] ])
+
+
+    metadata['xml-resolution-agent'] = agent(:id => Daitss::CONFIG['xmlresolution-url'],
+                                             :name => 'daitss xmlresolution service',
+                                             :type => 'software')
+
+  end
+
+  def put_collection_resource
+    url = URI.parse "#{Daitss::CONFIG['xmlresolution-url']}/ieids/#{id}"
     req = Net::HTTP::Put.new url.path
 
     res = Net::HTTP.start(url.host, url.port) do |http|
@@ -15,14 +33,28 @@ class Wip
 
     res.error unless Net::HTTPSuccess === res
 
-    datafiles.each do ||
+    url
+  end
+
+  def resolve_datafiles url
+    dfs = all_datafiles.select { |df| df.xmlresolution }
+
+    dfs.each do |df|
+      %x{curl -s -F xmlfile=@#{df.datapath} #{url}}
     end
 
-    # ask for the tarball
+  end
 
-    metadata['xml-resolution-tarball'] = 0
-    metadata['xml-resolution-event'] = 0
-    metadata['xml-resolution-agent'] = 0
+  def get_tarball url
+    req = Net::HTTP::Get.new url.path
+
+    res = Net::HTTP.start(url.host, url.port) do |http|
+      http.read_timeout = Daitss::CONFIG['http-read-timeout']
+      http.request req
+    end
+
+    res.error unless Net::HTTPSuccess === res
+    res.body
   end
 
 end
--- spec/wip/xmlresolution_spec.rb
+++ spec/wip/xmlresolution_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
+require 'datafile/describe'
 require 'wip/xmlresolve'
 
 describe Wip do
 
   before :all do
-    @wip = submit_sip 'mimi'
-    @wip.xmlresolve
+    @wip = submit 'mimi'
+    @wip.original_datafiles.each { |df| df.describe! }
+    @wip.xmlresolve!
   end
 
   it "should have a xml resolution tarball" do
@@ -18,6 +20,14 @@ describe Wip do
 
   it "should have an xml resolution agent" do
     @wip.should have_key('xml-resolution-agent')
+  end
+
+  it "should cleanup the tarball" do
+    pending "deletion is not implemented in xmlresolution service"
+    url = URI.parse "#{Daitss::CONFIG['xmlresolution-url']}/ieids/#{@wip.id}/"
+    req = Net::HTTP::Head.new url.path
+    res = Net::HTTP.start(url.host, url.port) { |http| http.request req }
+    res.should be_a_kind_of(Net::HTTPNotFound)
   end
 
 end
