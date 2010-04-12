894c35f0f4d3bc063c62b0cde1c6b78bd88118e3
Francesco Lazzarino <flazzarino@gmail.com>
2010-04-12 10:12:30 -0400

    reworked to be simpler, and testeding xmlres of a df

--- spec/datafile/actionplan_spec.rb
+++ spec/datafile/actionplan_spec.rb
@@ -3,28 +3,72 @@ require 'xmlns'
 require 'datafile/describe'
 require 'datafile/actionplan'
 
-describe 'action planning a datafile' do
+describe DataFile do
 
-  subject do
-    wip = submit 'wave'
-    df = wip.original_datafiles.find { |df| df['sip-path'] == 'obj1.wav' }
-    df.describe!
-    df
+  describe 'when the actionplan server is not available' do
+
+    it 'should raise an error' do
+
+      override_service 'actionplan-url', 500 do
+        lambda { subject.normalization }.should raise_error
+      end
+
+    end
+
   end
 
-  it "should return nil if there is no migration" do
-    subject.migration.should be_nil
+  describe "with no preservation actions" do
+
+    before :all do
+      wip = submit 'mimi'
+      @pdf = wip.original_datafiles.find { |df| df['aip-path'] == 'mimi.pdf' }
+      @pdf.describe!
+    end
+
+    it 'should return nil if there is no migration' do
+      @pdf.migration.should be_nil
+    end
+
+    it 'should return nil if there is no normalization' do
+      @pdf.normalization.should be_nil
+    end
+
+    it 'should return nil if there is no xmlresolution' do
+      @pdf.xmlresolution.should be_nil
+    end
+
   end
 
-  it "should redirect if there is a transformation" do
-    subject.normalization.should == 'http://localhost:7000/transformation/transform/wave_norm'
+  describe 'with a migration' do
+    it 'should redirect to a transformation'
   end
 
-  it "should raise an error is the the configuration is wrong" do
-    real_actionplan_url = Daitss::CONFIG['actionplan-url']
-    Daitss::CONFIG['actionplan-url'] = 'http://localhost:7000/statusecho/500'
-    lambda { subject.normalization }.should raise_error
-    Daitss::CONFIG['actionplan-url'] = real_actionplan_url
+  describe 'with a normalization' do
+
+    before :all do
+      wip = submit 'wave'
+      @wave = wip.original_datafiles.find { |df| df['aip-path'] == 'obj1.wav' }
+      @wave.describe!
+    end
+
+    it 'should redirect to a transformation' do
+      url = 'http://localhost:7000/transformation/transform/wave_norm'
+      @wave.normalization.should == url
+    end
+
   end
 
+  describe 'with a xmlresolution' do
+
+    before :all do
+      wip = submit 'wave'
+      @xml = wip.original_datafiles.find { |df| df['aip-path'] == 'wave.xml' }
+      @xml.describe!
+    end
+
+    it 'should not be nil' do
+      @xml.xmlresolution.should_not be_nil
+    end
+
+  end
 end
