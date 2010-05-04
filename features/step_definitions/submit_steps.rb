0daf300b691be25c5a624c7390bae5169d70ab54
Francesco Lazzarino <flazzarino@gmail.com>
2010-05-04 18:16:58 -0400

    submit steps

--- features/step_definitions/submit_steps.rb
+++ features/step_definitions/submit_steps.rb
@@ -9,3 +9,22 @@ Given /^xI submit (a|\d+) sips?$/ do |count|
   count.times { submit 'ateam' }
 
 end
+
+When /^I select a sip to upload$/ do
+  name = 'ateam'
+  tar = sip_tarball(name)
+  tio = Tempfile.open 'cuke'
+  dir = tio.path
+  tio.close!
+  FileUtils.mkdir dir
+  tar_file = File.join dir, "#{name}.tar"
+  open(tar_file, 'w') { |o| o.write tar }
+  attach_file 'sip', tar_file
+  $cleanup << dir
+end
+
+Then /^I should be at a wip page$/ do
+  #debugger
+  #redirected_to
+  last_request.path.should =~ %r{/workspace/\w+}
+end
