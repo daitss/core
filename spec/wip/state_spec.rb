7a4ac6313c5db065ba0ad5aea6de446f672d4ee9
Francesco Lazzarino <flazzarino@gmail.com>
2010-05-18 16:04:32 -0400

    dead wips -> snafu

--- spec/wip/state_spec.rb
+++ spec/wip/state_spec.rb
@@ -1,4 +1,30 @@
 require 'spec_helper'
 require 'wip/state'
 
+describe Wip do
 
+  it 'should have dead wips be snafu' do
+
+    id = UUID.generate :compact
+    uri = "bogus:/#{id}"
+    wip =blank_wip id, uri
+
+    wip.should_not be_running
+    wip.start { sleep }
+    wip.should be_running
+
+    wip.instance_eval do
+      pid, ptime = process
+
+      begin
+        loop { Process.kill "KILL", pid.to_i }
+      rescue Errno::ESRCH => e
+        # nothing to do, best way to detect no proc
+      end
+
+    end
+
+    wip.state.should == 'snafu'
+  end
+
+end
