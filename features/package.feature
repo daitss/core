c13e6fb32a10b4e73637970c4bf83e3a87d90869
Francesco Lazzarino <flazzarino@gmail.com>
2010-05-18 17:42:09 -0400

    aip metadata displayed

--- features/package.feature
+++ features/package.feature
@@ -23,10 +23,10 @@ Feature: overview of a package
     Then in the events I should see a submission event
 
   Scenario: show the aip
-    Given I submit a package
+    Given an archived package
     When I goto its package page
+    Then in the aip section I should see a link to the descriptor
     Then in the aip section I should see copy url
     Then in the aip section I should see copy size
     Then in the aip section I should see copy sha1
-    Then in the aip section I should see xml
     Then in the aip section I should see number of datafiles
--- features/step_definitions/package_steps.rb
+++ features/step_definitions/package_steps.rb
@@ -1,3 +1,9 @@
+Given /^an archived package$/ do
+  wip = submit 'mimi'
+  wip.start_task
+  sleep 0.5 until !wip.running?
+end
+
 When /^I goto its package page$/ do
   id = sips.last[:wip]
   visit "/package/#{id}"
@@ -16,7 +22,11 @@ Then /^in the events I should see a submission event$/ do
   last_response.should have_selector("td:contains('Package Submission')")
 end
 
-Then /^in the aip section I should see (copy url|copy size|copy sha1|xml|number of datafiles)$/ do |field|
-  pending
+Then /^in the aip section I should see (copy url|copy size|copy sha1|number of datafiles)$/ do |field|
   last_response.should have_selector("th:contains('#{field}') + td")
+end
+
+Then /^in the aip section I should see a link to the descriptor$/ do
+  id = sips.last[:wip]
+  last_response.should have_selector("h3 a[href='/package/#{id}/descriptor']:contains('xml descriptor')")
 end
