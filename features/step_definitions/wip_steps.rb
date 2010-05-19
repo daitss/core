f22fc1959027360c1b7d95f4a078566f2cf1eed5
Francesco Lazzarino <flazzarino@gmail.com>
2010-05-19 17:20:25 -0400

    wips are updatable

---
+++ features/step_definitions/wip_steps.rb
@@ -0,0 +1,13 @@
+Given /^I goto its wip page$/ do
+  id = sips.last[:wip]
+  Given %Q(I goto "/workspace/#{id}")
+end
+
+Then /^the package should be (don't know|running|stopped|idle)$/ do |status|
+
+  unless status == "don't know"
+    last_response.should have_selector("th:contains('status') + td:contains('#{status}')")
+  end
+
+end
+
