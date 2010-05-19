7b799c33bf2c71ae23bdaf580d5ab855b34ef56d
Francesco Lazzarino <flazzarino@gmail.com>
2010-05-19 14:36:28 -0400

    stashbin resource

---
+++ lib/stashbin.rb
@@ -0,0 +1,26 @@
+require 'daitss/config'
+
+class StashBin
+  include Daitss
+  include DataMapper::Resource
+
+  property :name, String, :key => true
+
+  def wips
+    pattern = File.join path, '*'
+    Dir[pattern].map { |p| Wip.new p }
+  end
+
+  def url_name
+    URI.escape name
+  end
+
+  def sha1
+    Digest::SHA1.hexdigest name
+  end
+
+  def path
+    File.join CONFIG['stashspace'], sha1
+  end
+
+end
