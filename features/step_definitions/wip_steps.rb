495f1358763ad571d375a715db78d68fe576b9e2
Francesco Lazzarino <flazzarino@gmail.com>
2010-05-21 10:59:20 -0400

    wip progress

--- features/step_definitions/wip_steps.rb
+++ features/step_definitions/wip_steps.rb
@@ -11,3 +11,10 @@ Then /^the package should be (don't know|running|stopped|idle)$/ do |status|
 
 end
 
+Then /^I should see the progress section$/ do
+  last_response.should have_selector("h2:contains('progress')")
+end
+
+Then /^in the progress section I should see a field for "([^\"]*)"$/ do |field|
+  last_response.should have_selector("th:contains('#{field}')")
+end
