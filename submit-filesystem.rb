3cd24d4f7053389c196b18433eecac41ea967a82
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-07-07 10:44:54 -0400

    simple test commit to figure out why permissions are changing on retsina

--- submit-filesystem.rb
+++ submit-filesystem.rb
@@ -6,6 +6,7 @@ require 'ostruct'
 require 'digest/md5'
 require 'fileutils'
 
+
 # a simple submission client
 # takes a specified package on a filesystem, tars it up, and "curls" it to the submission at the specified url as the specified
 #    operations agent.
