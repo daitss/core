de3d9149dcf6e4c20a2d56361864a2c4deb59997
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-07-13 15:47:00 -0400

    2**63 is the largest integer postgres can handle

--- lib/db/sip.rb
+++ lib/db/sip.rb
@@ -5,8 +5,8 @@ class SubmittedSip
 
   property :id, Serial, :key => true
   property :package_name, String, :key => true
-  property :package_size, Integer, :min => 0, :max => 2**64-1
-  property :number_of_datafiles, Integer, :min => 0, :max => 2**64-1
+  property :package_size, Integer, :min => 0, :max => 2**63-1
+  property :number_of_datafiles, Integer, :min => 0, :max => 2**63-1
   property :ieid, String
 
   has n, :operations_events
