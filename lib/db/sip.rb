15abd4066982f3f4a269df412af83019c0993094
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-07-13 15:12:42 -0400

    need bigints on package size and number of datafiles fields

--- lib/db/sip.rb
+++ lib/db/sip.rb
@@ -5,8 +5,8 @@ class SubmittedSip
 
   property :id, Serial, :key => true
   property :package_name, String, :key => true
-  property :package_size, Integer
-  property :number_of_datafiles, Integer
+  property :package_size, Integer, :min => 0, :max => 2**64-1
+  property :number_of_datafiles, Integer, :min => 0, :max => 2**64-1
   property :ieid, String
 
   has n, :operations_events
