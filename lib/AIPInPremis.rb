534f24e91fe916ec9ab906021b5ace1396145d82
Francesco Lazzarino <flazzarino@gmail.com>
2010-07-30 16:11:40 -0400

    silence the puts

--- lib/AIPInPremis.rb
+++ lib/AIPInPremis.rb
@@ -223,11 +223,11 @@ class AIPInPremis
     # datamapper automatically rollback the change.
     # RubyProf.start
 #   debugger
- 	if @int_entity.save
-       puts "successfully save int entity into the preservation database"
-	 else
- 		raise "cannot save aip"
-	end
+    if @int_entity.save
+    #  puts "successfully save int entity into the preservation database"
+    else
+      raise "cannot save aip"
+    end
    # @int_entity.save or raise "cannot save aip"
     @acctProject.save
     # not necessary to explicitely save representations since representations will be saved through intentity associations
