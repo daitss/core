c0ad4f55bb3991a8884e8d3b8b0aa6ccad1f8668
Francesco Lazzarino <flazzarino@gmail.com>
2010-08-03 14:52:19 -0400

    simplification of css refactor wip table

--- features/step_definitions/wip_steps.rb
+++ features/step_definitions/wip_steps.rb
@@ -12,5 +12,5 @@ Then /^the package should be (don't know|running|stopped|idle)$/ do |status|
 end
 
 Then /^in the progress section I should see a field for "([^\"]*)"$/ do |field|
-  last_response.should have_selector("th:contains('#{field}')")
+  last_response.should have_selector("td:contains('#{field}')")
 end
