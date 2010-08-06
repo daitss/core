a82cfd0b0ec68354abe5c365112876bdb08579cf
Francesco Lazzarino <flazzarino@gmail.com>
2010-08-06 18:39:57 -0400

    moving templates in lib

--- lib/daitss/proc/template.rb
+++ lib/daitss/proc/template.rb
@@ -2,7 +2,10 @@ require 'erb'
 
 include ERB::Util
 
+TEMPLATE_DIR = File.expand_path File.join(File.dirname(__FILE__), 'template', 'erb')
+
 def template_by_name name
-  file = File::expand_path File::join(File.dirname(__FILE__), '..', 'templates', "#{name}.erb")
-  Kernel::open(file) { |io| ERB.new io.read }
+  file = File.join TEMPLATE_DIR, "#{name}.erb"
+  raw = File.read file
+  ERB.new raw
 end
