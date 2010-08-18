80c4e4c0b7b9104fe5b55d7c035c66a8e71abc37
Francesco Lazzarino <flazzarino@gmail.com>
2010-08-18 11:35:42 -0400

    DateTime#pragma now in core

---
+++ lib/daitss/datetime.rb
@@ -0,0 +1,24 @@
+require 'date'
+
+class DateTime
+
+  # returns a string representation of the DateTime object
+  #
+  # if the object is ...
+  # within the current day: '%I:%M %p'
+  # within the current year: '%b %d'
+  # anything else: '%D'
+  def pragma
+    now = DateTime.now
+
+    if now.jd == self.jd
+      self.strftime('%I:%M %p').downcase
+    elsif now.year == self.year
+      self.strftime '%b %d'
+    else
+     self.strftime '%D'
+    end
+
+  end
+
+end
