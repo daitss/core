db1ef79832df2935e88c2dca8f71409256d27aa3
Francesco Lazzarino <flazzarino@gmail.com>
2010-08-18 17:32:01 -0400

    sip archive validation complete

--- lib/daitss/proc/sip_archive.rb
+++ lib/daitss/proc/sip_archive.rb
@@ -6,31 +6,112 @@ include LibXML
 # Provides access to an archive (zip, tar) sip
 class SipArchive
 
-  attr_reader :path, :owner_ids, :account, :project, :title, :issue, :volume, :entity_id
+  MAX_NAME_LENGTH = 32
+
+  attr_reader :path, :errors, :owner_ids, :account, :project, :title, :issue, :volume, :entity_id
 
   def initialize path
     path = File.expand_path path
 
     filename = File.basename path
+    raise "invalid characters in sip name" if filename =~ /^\..*['" ]/
+
     ext = File.extname path
     name = File.basename path, ext
-
+    raise "package name contains too many characters (#{name.length}) max is #{MAX_NAME_LENGTH}" if name.length > MAX_NAME_LENGTH
 
     Dir.chdir File.dirname(path) do
 
-      case ext
-      when '.zip' then `unzip -o #{filename}`
-      when '.tar' then `tar -xf #{filename}`
-      else raise "unknown archive extension: #{ext}"
-      end
+      output = case ext
+               when '.zip' then `unzip -o #{filename} 2>&1`
+               when '.tar' then `tar -xf #{filename} 2>&1`
+               else raise "unknown archive extension: #{ext}"
+               end
 
-      raise "error extracting #{filename}\n#{output}" unless $? == 0
+      raise "error extracting #{filename}\n\n#{output}" unless $? == 0
     end
 
     @name = name
     @path = File.join File.dirname(path), name
 
     raise "#{filename} is not a package" unless File.directory? @path
+  end
+
+  def valid?
+    validate! unless @errors
+    @errors.empty?
+  end
+
+
+  def validate!
+    es = Hash.new { |h,k| h[k] = [] }
+
+    # check for missing descriptor
+    es[:descriptor_presence] << "missing descriptor" unless File.file? descriptor_file
+
+    # check for valid descriptor
+    if es[:descriptor_presence].empty?
+      xml = File.read descriptor_file
+      val = JXML::Validator.new
+      results = val.validate xml
+      validation_errors = results[:errors] + results[:fatals]
+
+      unless validation_errors.empty?
+        es[:descriptor_valid] << "invalid descriptor"
+        es[:descriptor_valid] += validation_errors.map { |e| "#{e[:line]}: #{e[:message]}" }
+      end
+
+    end
+
+    # check for content files
+    if es[:descriptor_presence].empty? and es[:descriptor_valid].empty?
+      es[:content_file_presence] << "missing content files" if content_files.empty?
+    end
+
+    # check content file name validity
+    if es[:descriptor_presence].empty? and es[:descriptor_valid].empty? and es[:content_file_presence].empty?
+
+      content_files.each do |f|
+        es[:content_file_name_validity] << "invalid characters in file name: #{f}" if f =~ /^\..*['" ]/
+      end
+
+    end
+
+    # check content file fixity
+    if es[:descriptor_presence].empty? and es[:descriptor_valid].empty? and es[:content_file_presence].empty?
+
+      Dir.chdir @path do
+
+        content_files_with_checksums.each do |f, expected, expected_type|
+
+          # try to infer expected type if not provided
+          if expected_type.nil? or expected_type.empty?
+
+            expected_type = case expected
+                            when %r{^[a-fA-F0-9]{32}$} then 'MD5'
+                            when %r{^[a-fA-F0-9]{40}$} then 'SHA-1'
+                            end
+
+          end
+
+          # compute the checksum
+          computed = case expected_type
+                     when "MD5" then Digest::MD5.file(f).hexdigest
+                     when "SHA-1" then Digest::SHA1.file(f).hexdigest
+                     else next
+                     end
+
+          if computed != expected
+            es[:content_file_fixity] << "#{expected_type} for #{f} - expected: #{expected}; computed #{computed}"
+          end
+
+        end
+
+      end
+
+    end
+
+    @errors = es.values.flatten
   end
 
   def old_initialize path
@@ -92,17 +173,37 @@ class SipArchive
     return root_node["OBJID"] ? root_node["OBJID"] : nil
   end
 
-  def descriptor_file
-    descriptor_file = File.join @path, "#{name}.xml"
+  def descriptor_doc
+    @descriptor_doc ||= XML::Document.string File.read(descriptor_file)
   end
 
   def name
     File.basename @path
   end
 
+  def descriptor_file
+    descriptor_file = File.join @path, "#{name}.xml"
+  end
+
+  def content_files_with_checksums
+    ns = descriptor_doc.find "//M:file", NS_PREFIX
+
+    ns.map do |n|
+      path = n.find_first("M:FLocat/@xlink:href", NS_PREFIX).value
+      cs = n['CHECKSUM']
+      cst = n['CHECKSUMTYPE']
+      [path, cs, cst]
+    end
+
+  end
+
+  def content_files
+    ns = descriptor_doc.find "//M:file/M:FLocat/@xlink:href", NS_PREFIX
+    ns.map { |n| n.value }
+  end
+
   def files
-    ns = @descriptor_doc.find "//M:file/M:FLocat/@xlink:href", NS_PREFIX
-    ns.map { |n| n.value } + [File.basename(descriptor_file)]
+    [ File.basename(descriptor_file) ] + content_files
   end
 
 end
