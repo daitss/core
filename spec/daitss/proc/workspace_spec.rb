526630011ca05014df9175961e5f682c63aa317a
Francesco Lazzarino <flazzarino@gmail.com>
2010-08-20 10:57:41 -0400

    workspace specs passing

--- spec/daitss/proc/workspace_spec.rb
+++ spec/daitss/proc/workspace_spec.rb
@@ -5,33 +5,33 @@ require 'daitss/db/ops/stashbin'
 describe Workspace do
 
   it "should know if it has a specific wip" do
-    w = Workspace.new $sandbox
-    wip = submit 'mimi', w
+    w = Archive.new.workspace
+    wip = submit 'mimi'
     w.should have_wip(wip.id)
     w.should_not have_wip('xxx')
   end
 
   it "should list packages" do
-    w = Workspace.new $sandbox
-    wips = %w(mimi haskell-nums-pdf).map { |name| submit name, w }
+    w = Archive.new.workspace
+    wips = %w(mimi haskell-nums-pdf).map { |name| submit name }
     w.should include(*wips)
   end
 
   it "should select packages by id" do
-    w = Workspace.new $sandbox
-    wip = submit 'mimi', w
+    w = Archive.new.workspace
+    wip = submit 'mimi'
     w[wip.id].should == wip
   end
 
   it "should stash & unstash a package" do
     bin = StashBin.new :name => 'test bin'
-    ws = Workspace.new Daitss::CONFIG['workspace']
-    wip = submit 'mimi', ws
+    w = Archive.new.workspace
+    wip = submit 'mimi'
     wip_id = wip.id
-    ws.stash wip.id, bin
-    ws[wip_id].should be_nil
+    w.stash wip.id, bin
+    w[wip_id].should be_nil
     bin.unstash wip_id
-    ws[wip_id].should_not be_nil
+    w[wip_id].should_not be_nil
   end
 
 end
--- spec/help/test_package.rb
+++ spec/help/test_package.rb
@@ -1,53 +1,18 @@
 require "uuid"
 
+require "daitss/archive"
 require "daitss/proc/workspace"
 require "daitss/proc/wip/from_sip"
 require "daitss/proc/template/premis"
 
 SIPS_DIR = File.join File.dirname(__FILE__), '..', 'sips'
 
-def submit sip_name
-  wip_id = UUID.generate :compact
-
-
-  workspace = Workspace.new $sandbox if workspace.nil?
-
-  # make the sip
-  sip_path = File.join SIPS_DIR, sip_name
-  sip = SipArchive.new sip_path
-
-  # add a sip record
-  Dir.chdir sip.path do
-    ss = Sip.new
-    ss.package_name = sip.name
-    ss.package_size = sip.files.inject(0) { |sum, f| sum + File.size(f) }
-    ss.number_of_datafiles = sip.files.size
-    ss.ieid = wip_id
-    ss.save or raise "cannot make submitted sip"
-  end
-
-  # make a staging area
-  FileUtils.mkdir_p File.join(workspace.path, '.tmp')
-
-  # make a wip in the staging area
-  path = File.join workspace.path, '.tmp', wip_id
-  uri = "#{Daitss::CONFIG['uri-prefix']}/#{wip_id}"
-  wip = Wip.from_sip path, uri, sip
-
-  wip['submit-event'] = event(:id => "#{wip.uri}/event/submit",
-                              :type => 'submit',
-                              :outcome => 'success',
-                              :linking_objects => [ wip.uri ],
-                              :linking_agents => [ 'info:fcla/daitss/reference-submit-client' ])
-
-  wip['submit-agent'] = agent(:id => 'info:fcla/daitss/reference-submit-client',
-                              :name => 'daitss submission service',
-                              :type => 'software')
-
-  # move the wip into the workspace
-  new_path = File.join workspace.path, wip_id
-  FileUtils.mv wip.path, new_path
-  Wip.new new_path
+def submit name
+  zip_path = File.join SIP_ARCHIVE_DIR, "#{name}.zip"
+  agent = OperationsAgent.first :identifier => 'Bureaucrat'
+  a = Archive.new
+  sip = a.submit zip_path, agent
+  a.workspace[sip.id]
 end
 
 def blank_wip id, uri
