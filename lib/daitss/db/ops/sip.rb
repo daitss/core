d374e455637a4aaa3b0d1a4383f064e5dfa5a9c5
Francesco Lazzarino <flazzarino@gmail.com>
2010-08-20 15:43:39 -0400

    reject ops events are specced

--- lib/daitss/db/ops/sip.rb
+++ lib/daitss/db/ops/sip.rb
@@ -16,7 +16,6 @@ class EggHeadKey < DataMapper::Property::String
   #
   # TODO need something more entropic than time.
   #      payout of improving this is a decent payoff
-  # TODO make this automagic for id?
   def EggHeadKey.new_egg_head_key
     string = ::Time.now.to_f.to_s.gsub(".", "").to_i.to_s(36).upcase
 
@@ -47,51 +46,18 @@ class Sip
   def Sip.from_sip_archive sa
     sip = Sip.new
     sip.name = sa.name
-    sip.number_of_datafiles = sa.files.size
 
-    sip.size_in_bytes = sa.files.inject(0) do |sum, f|
-      path = File.join sa.path, f
-      sum + File.size(path)
+    if sa.valid?
+      sip.number_of_datafiles = sa.files.size
+
+      sip.size_in_bytes = sa.files.inject(0) do |sum, f|
+        path = File.join sa.path, f
+        sum + File.size(path)
+      end
+
     end
 
     sip
-  end
-
-  def Sip.submit_from_archive workspace, archive_path, id_addr, op_agent
-    sip = Sip.new
-
-    wip_path = File.join workspace.submit_dir, sip.id
-    wip_uri = URI_PREFIX + sip.id
-
-    @errors = []
-    @op_event_notes = "submitter_ip: #{ip_addr};"
-    @agent = op_agent
-
-    begin
-      sa = SipArchive.from_archive archive_path, sip.id, package_name
-      wip = Wip.from_sip wip_path, wip_uri, sa
-
-      raise SipDescriptorInvalid unless wip.sip_descriptor_valid?
-    rescue DescriptorNotFoundError
-      @errors.push REJECT_DESCRIPTOR_NOT_FOUND
-      reject ieid
-    rescue ArchiveExtractionError
-      @errors.push REJECT_ARCHIVE_EXTRACTION_ERROR
-      reject ieid
-    rescue LibXML::XML::Error, SipDescriptorInvalid
-      @errors.push REJECT_INVALID_DESCRIPTOR
-      reject ieid
-    else
-      validate wip
-      add_project_to_sip_record wip
-      reject ieid if @errors.any?
-      wip.trim_undescribed_datafiles
-      write_metadata wip
-      wip.task = :ingest
-      write_success_op_event ieid
-      FileUtils.mv wip_path, File.join(@workspace.path, ieid)
-    end
-
   end
 
 end
