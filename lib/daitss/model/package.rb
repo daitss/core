153454d804492535f7a02ca453bc1e32b50e6702
Francesco Lazzarino <flazzarino@gmail.com>
2010-08-24 20:09:41 -0400

    Package is the record of a package
    it ties wips, sips, aips, etc together
    egghead key factored out

---
+++ lib/daitss/model/package.rb
@@ -0,0 +1,34 @@
+require 'data_mapper'
+
+require 'daitss/model/aip'
+require 'daitss/model/eggheadkey'
+require 'daitss/model/event'
+
+# authoritative package record
+class Package
+  include DataMapper::Resource
+
+  property :id, EggHeadKey
+
+  has n, :events
+  has n, :requests
+  has 1, :sip
+  has 0..1, :aips
+
+  belongs_to :project, :required => false
+
+  # add an operations event for abort
+  def abort user
+    event = Event.new :name => 'abort', sip => self, :agent => user
+    event.save or raise "cannot save op event"
+  end
+
+  # make an event for this package
+  def log name, options={}
+    e = Event.new :name => name, :sip => self
+    e.agent = options[:agent] || Program.system_agent
+    e.notes = options[:notes]
+    e.save or raise "cannot save op event: #{name}"
+  end
+
+end
--- lib/daitss/model/sip.rb
+++ lib/daitss/model/sip.rb
@@ -1,72 +1,13 @@
 require 'data_mapper'
 
-require 'daitss/model/aip'
-require 'daitss/model/event'
-
-class EggHeadKey < DataMapper::Property::String
-  key true
-  default proc { |res, prop| EggHeadKey.new_egg_head_key }
-
-  # here's how the base string for the ieid is generated:
-  # get a floating point representation of the current time
-  # convert that floating point number to a string
-  # remove the decimal point
-  # convert the now decimal point less string into an integer object
-  # use Integer's to_s method to get a base 36 representation
-  #
-  # TODO need something more entropic than time.
-  #      payout of improving this is a decent payoff
-  def EggHeadKey.new_egg_head_key
-    string = ::Time.now.to_f.to_s.gsub(".", "").to_i.to_s(36).upcase
-
-    # pad with zeros to 14 characters
-    string = ("0" * (14 - string.length)) + string
-
-    # add underscore
-    string = string.insert(8, "_")
-    return "E" + string
-  end
-
-end
-
+# description of a submitted sip
 class Sip
   include DataMapper::Resource
 
-  property :id, EggHeadKey
-  property :name, String
+  property :id, Serial
+  property :name, String, :required => true
   property :size_in_bytes, Integer, :min => 0, :max => 2**63-1
   property :number_of_datafiles, Integer, :min => 0, :max => 2**63-1
 
-  has n, :operations_events
-  has n, :requests
-  #has 0..1, :aips
-
-  belongs_to :project, :required => false
-
-  def Sip.from_sip_archive sa
-    sip = Sip.new
-    sip.name = sa.name
-
-    if sa.valid?
-      sip.number_of_datafiles = sa.files.size
-
-      sip.size_in_bytes = sa.files.inject(0) do |sum, f|
-        path = File.join sa.path, f
-        sum + File.size(path)
-      end
-
-    end
-
-    sip
-  end
-
-  # add an operations event for abort
-  def abort user
-    event = OperationsEvent.new :event_name => 'abort'
-    event.operations_agent = user
-    event.sip = self
-    event.timestamp = Time.now
-    event.save or raise "cannot save op event"
-  end
-
+  belongs_to :package
 end
