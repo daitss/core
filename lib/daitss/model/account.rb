d932e67b92a233a9d64274ea53531ccfa5747fbb
Manny Rodriguez <manny@bianchi.local>
2010-09-03 12:26:46 -0400

    cleanup account and agent models, system_account and agent code moved to init script

--- lib/daitss/model/account.rb
+++ lib/daitss/model/account.rb
@@ -16,15 +16,4 @@ class Account
   has n, :agents
   has n, :requests
 
-  def Account.system_account
-    a = Account.first :id => 'SYSTEM'
-
-    unless a
-      a = Account.new :id => 'SYSTEM', :description => 'account for system operations'
-      a.save or raise "cannot save system account"
-    end
-
-    a
-  end
-
 end
