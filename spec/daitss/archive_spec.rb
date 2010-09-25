480bf18adb1026bb97a17cb511f42c3acaaf9f57
Francesco Lazzarino <flazzarino@gmail.com>
2010-09-25 18:55:51 -0400

    moved to higher level submission

--- spec/daitss/archive_spec.rb
+++ spec/daitss/archive_spec.rb
@@ -9,7 +9,7 @@ describe Daitss::Archive do
 
       before :all do
         @archive = Daitss::Archive.instance
-        path = new_sip_archive 'mimi.zip'
+        path = new_sip_archive 'haskell-nums-pdf.zip'
         user = User.get('Bureaucrat')
         @package = @archive.submit path, user
       end
@@ -24,6 +24,59 @@ describe Daitss::Archive do
         wip = @archive.workspace[@package.id]
         wip.should_not be_nil
         wip.task.should == :ingest
+      end
+
+      describe 'resulting wip' do
+
+        subject { Daitss::Archive.instance.workspace[@package.id] }
+
+        it "should have sip descriptor as metadata" do
+          sd_df = subject.original_datafiles.find { |df| df['sip-path'] == "#{subject['sip-name']}.xml" }
+          subject['sip-descriptor'].should == sd_df.open.read
+        end
+
+        it "should have 2 files" do
+          subject.original_datafiles.should have_exactly(2).items
+        end
+
+        it "should have sip-name in it" do
+          subject['sip-name'].should == 'haskell-nums-pdf'
+        end
+
+        it "all files should have a sip path" do
+          subject.original_datafiles[0]['sip-path'].should == 'haskell-nums-pdf.xml'
+          subject.original_datafiles[1]['sip-path'].should == 'Haskell98numbers.pdf'
+        end
+
+        it "all files should have a aip path" do
+          subject.original_datafiles[0]['aip-path'].should == File.join(AipArchive::SIP_FILES_DIR, 'haskell-nums-pdf.xml')
+          subject.original_datafiles[1]['aip-path'].should == File.join(AipArchive::SIP_FILES_DIR, 'Haskell98numbers.pdf')
+        end
+
+        it "should extract FDA account from the descriptor" do
+          subject.metadata["dmd-account"].should == "ACT"
+        end
+
+        it "should extract FDA project from the descriptor" do
+          subject.metadata["dmd-project"].should == "PRJ"
+        end
+
+        it "should extract title from the descriptor" do
+          subject.metadata["dmd-title"].should == "Haskell Numbers"
+        end
+
+        it "should extract issue from the descriptor" do
+          subject.metadata["dmd-issue"].should == "2"
+        end
+
+        it "should extract volume from the descriptor" do
+          subject.metadata["dmd-volume"].should == "1"
+        end
+
+        it "should extract entity id from descriptor" do
+          subject.metadata["dmd-entity-id"].should == "haskell-nums-pdf"
+        end
+
       end
 
     end
