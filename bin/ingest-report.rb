570b84ba3a879065d973bbc98d057efe55e5153c
Manny Rodriguez <manny@bianchi.fcla.edu>
2010-09-27 09:53:27 -0400

    event generator up to date with db changes, has datafile events

--- bin/ingest-report.rb
+++ bin/ingest-report.rb
@@ -3,7 +3,8 @@
 # takes an IEID as input, outputs XML equivalent to a DAITSS Ingest report.
 
 require 'rexml/document'
-require 'daitss2'
+require 'daitss/db'
+require 'daitss/model'
 require 'daitss/config'
 require 'time'
 require 'pp'
@@ -19,28 +20,10 @@ def fail message
   exit 1
 end
 
-#def write_file ieid, output
-  #filename_flat = File.join PATH_TO_WRITE_FILE, ieid + "_INGEST.xml"
-#
-  #output_file = File.new filename_flat, "w+"
-  #output_file << "<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>\n"
-  #output_file << "<?xml-stylesheet type=\"text/xsl\" href=\"daitss_report_xhtml.xsl\"?>\n"
-  #output_file << output
-#end
-
-# check that destination is a directory
-#if not File.directory? PATH_TO_WRITE_FILE
-  #fail "Error, #{PATH_TO_WRITE_FILE} is not a directory."
-#end
-#
-# check that destination is writable 
-#if not File.writable? PATH_TO_WRITE_FILE
-  #fail "Error, #{PATH_TO_WRITE_FILE} is not a writable."
-#end
-
 # get data from database to build report
-intentity_record = Intentity.first(:id => Daitss::CONFIG['uri-prefix'] + IEID)
-sip = SubmittedSip.first(:ieid => IEID)
+intentity_record = Intentity.first(:id => Daitss::CONFIG['uri-prefix'] + "/" + IEID)
+package = Package.first(:id => IEID)
+sip = package.sip
 
 if not intentity_record
   fail "Error, there is no record that #{ARGV[0]} was ingested. Please try again."
@@ -61,7 +44,7 @@ report_element.attributes["xsi:schemaLocation"] = "http://www.fcla.edu/dls/md/da
 ingest_element = REXML::Element.new "INGEST"
 
 ingest_element.attributes["IEID"] = intentity_record.id
-ingest_element.attributes["INGEST_TIME"] = sip.operations_events.first(:event_name => "Package Submission").timestamp.to_time.iso8601
+ingest_element.attributes["INGEST_TIME"] = Time.parse(package.events.first(:name => "submit").timestamp.to_s).iso8601
 ingest_element.attributes["PACKAGE"] = intentity_record.original_name
 
 report_element << ingest_element
@@ -71,8 +54,8 @@ agreement_info_element = REXML::Element.new "AGREEMENT_INFO"
 
 ingest_element << agreement_info_element
 
-agreement_info_element.attributes["ACCOUNT"] = intentity_record.project.account.code
-agreement_info_element.attributes["PROJECT"] = intentity_record.project.code
+agreement_info_element.attributes["ACCOUNT"] = package.project.account.id
+agreement_info_element.attributes["PROJECT"] = package.project.id
 
 # FILES element
 files_element = REXML::Element.new "FILES"
@@ -82,6 +65,7 @@ ingest_element << files_element
 intentity_record.datafiles.each do |datafile_record|
   file_element = REXML::Element.new "FILE"
   md_sha1_element = REXML::Element.new "MESSAGE_DIGEST"
+  md_md5_element = REXML::Element.new "MESSAGE_DIGEST"
   
   file_element.attributes["DFID"] = datafile_record.id
   file_element.attributes["ORIGIN"] = datafile_record.origin
@@ -89,10 +73,13 @@ intentity_record.datafiles.each do |datafile_record|
   file_element.attributes["SIZE"] = datafile_record.size
 
   md_sha1_element.attributes["ALGORITHM"] = "SHA-1"
+  md_sha1_element.text = datafile_record.message_digest.first(:code => "SHA-1").value
 
-  md_sha1_element.text = datafile_record.message_digest.first(:code => :sha1).value
+  md_md5_element.attributes["ALGORITHM"] = "MD5"
+  md_md5_element.text = datafile_record.message_digest.first(:code => "MD5").value
 
   file_element << md_sha1_element
+  file_element << md_md5_element
   files_element << file_element
 
   # add broken link element if it exists
@@ -119,25 +106,23 @@ intentity_record.datafiles.each do |datafile_record|
     end
   end
 
-  # TODO: add datafile EVENTS
-  #if file_hash[1]["EVENTS"].length > 0
-    #file_hash[1]["EVENTS"].each do |event|
-      #event_element = REXML::Element.new "EVENT"
-      #procedure_element = REXML::Element.new "PROCEDURE"
-      #note_element = REXML::Element.new "NOTE"
-#
-      #event_element.attributes["TIME"] = event.DATE_TIME
-      #event_element.attributes["OUTCOME"] = event.OUTCOME
-#
-      #note_element.text = event.NOTE
-      #procedure_element.text = event.EVENT_PROCEDURE
-#
-      #event_element << note_element
-      #event_element << procedure_element
-#
-      #file_element << event_element
-    #end
-  #end
+  # add datafile EVENTS
+  PreservationEvent.all(:relatedObjectId => datafile_record.id, :class => DatafileEvent).each do |event|
+      event_element = REXML::Element.new "EVENT"
+      procedure_element = REXML::Element.new "PROCEDURE"
+      note_element = REXML::Element.new "NOTE"
+
+
+      event_element.attributes["TIME"] = Time.parse(event.datetime.to_s).iso8601
+      event_element.attributes["OUTCOME"] = event.outcome
+      procedure_element.text = event.e_type
+      note_element.text = event.outcome_details
+
+      event_element << note_element
+      event_element << procedure_element
+
+      file_element << event_element
+  end
 end
 
 report.add_element report_element
@@ -147,4 +132,3 @@ pretty_printer = REXML::Formatters::Pretty.new
 pretty_printer.write(report, pretty_output)
 
 puts pretty_output
-#write_file ARGV[0], pretty_output
