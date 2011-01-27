c53fdd7bbd0960a84b88c1bc93dae53100d18933
Manny Rodriguez <manny@bianchi.fcla.edu>
2011-01-27 17:32:57 -0500

    2^63 is too big, should be (2^63)-1

--- lib/daitss/model/copy.rb
+++ lib/daitss/model/copy.rb
@@ -13,7 +13,7 @@ module Daitss
     property :url, URI, :required => true # uncomment after all d1 packages are migrated, , :writer => :private #, :default => proc { self.make_url }
     property :sha1, String, :length => 40, :format => %r([a-f0-9]{40}) # uncomment after all d1 packages are migrated, :required => true
     property :md5, String, :length => 40, :format => %r([a-f0-9]{32}), :required => true
-    property :size, Integer, :min => 1, :max => 2**63 # uncomment after all d1 packages are migrated,:required => true
+    property :size, Integer, :min => 1, :max => (2**63)-1 # uncomment after all d1 packages are migrated,:required => true
     property :timestamp, Time
 
     belongs_to :aip
