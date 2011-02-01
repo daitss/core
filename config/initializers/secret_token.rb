cc1e235d3af0a6c6877686863802f5cfe33c28e2
Francesco Lazzarino <flazzarino@gmail.com>
2011-02-01 15:44:22 -0500

    initial rails app
    rails new daitss -m http://datamapper.org/templates/rails.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Daitss::Application.config.secret_token = '3bbbbaae8db81a5e28bc13b6f76851cc804aa8aa081df733ca667def6011aecd081161591da7856c4303f666e1e40c2a072e08638c4acd1d2a7798f915ed5d0d'
