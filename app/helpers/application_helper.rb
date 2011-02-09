e0d180da63af259405a1604ebdad2613ae9f0f95
Francesco Lazzarino <flazzarino@gmail.com>
2011-02-09 16:12:14 -0500

    breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def breadcrumbs
+    html = []
+
+    html << link_to("daitss", root_path)
+
+    case controller.controller_name
+    when 'settings'
+      html << link_to('settings', settings_path)
+    end
+
+    html.join(' &raquo; ').html_safe
+  end
+
 end
