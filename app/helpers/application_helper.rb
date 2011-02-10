f09331ea02ea1e6b9811aff454a560d745626da8
Francesco Lazzarino <flazzarino@gmail.com>
2011-02-10 10:57:33 -0500

    admin logs is in the breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,9 @@ module ApplicationHelper
     case controller.controller_name
     when 'settings'
       html << link_to('settings', settings_path)
+    when 'adminlogs'
+      html << link_to('admin logs', adminlogs_path)
+
     end
 
     html.join(' &raquo; ').html_safe
