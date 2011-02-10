da7fb7221eb120f7e178f88cbe5f49d1b2d0bd89
Francesco Lazzarino <flazzarino@gmail.com>
2011-02-10 10:59:30 -0500

    clean up new setting page from cuke paths.rb

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,9 +10,6 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/'
-    when /the new setting page/
-      new_setting_path
-
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
