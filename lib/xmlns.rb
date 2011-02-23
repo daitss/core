121badc9d8ffac022750d813481a9f442a70e587
Francesco Lazzarino <flazzarino@gmail.com>
2011-02-23 16:53:51 -0500

    moving xmlns from old app

---
+++ lib/xmlns.rb
@@ -0,0 +1,28 @@
+NS_PREFIX = {
+  'P' => 'info:lc/xmlns/premis-v2',
+  'M' => 'http://www.loc.gov/METS/',
+  'xlink' => 'http://www.w3.org/1999/xlink',
+  'xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+  'beta' => 'info:lc/xmlns/premis-v2-beta',
+  'mods' => 'http://www.loc.gov/mods/v3',
+  'daitss' => 'http://www.fcla.edu/dls/md/daitss/',
+  'aes' => 'http://www.aes.org/audioObject',
+  'marc' => 'http://www.loc.gov/MARC21/slim',
+  'dc' => 'http://purl.org/dc/elements/1.1/'
+}
+
+NAMESPACES = {
+  'mets' => 'http://www.loc.gov/METS/',
+  'xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+  'premis' => 'info:lc/xmlns/premis-v2',
+  'mix' => 'http://www.loc.gov/mix/v20',
+  'aes' => 'http://www.aes.org/audioObject',
+  'tcf' => 'http://www.aes.org/tcf',
+  'txt' => 'http://www.loc.gov/standards/textMD',
+  'doc' => 'http://www.fcla.edu/dls/md/docmd',
+  'p2' => 'info:lc/xmlns/premis-v2-beta',
+  'mods' => 'http://www.loc.gov/mods/v3',
+  'daitss' => 'http://www.fcla.edu/dls/md/daitss/',
+  'marc' => 'http://www.loc.gov/MARC21/slim',
+  'dc' => 'http://purl.org/dc/elements/1.1/'
+}
