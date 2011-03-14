60a214da878f4551529321e2e150f07ecf2dabec
Carol Chou <cchou@ufl.edu>
2011-03-14 12:09:18 -0400

    increase volumn, issues to have maximum 64 chars

--- lib/daitss/db/int_entity.rb
+++ lib/daitss/db/int_entity.rb
@@ -10,8 +10,8 @@ module Daitss
     property :original_name, String, :length => 32, :required => true, :default => "UNKNOWN"
     # i.e. package_name
     property :entity_id, String, :length => 100
-    property :volume, String, :length => 32
-    property :issue, String, :length => 32
+    property :volume, String, :length => 64
+    property :issue, String, :length => 64
     property :title, Text
 
     belongs_to :package
