13cc7d2d493135b06478d7db398ea48d743966fc
Manny Rodriguez <manny@bianchi.fcla.edu>
2011-03-25 15:57:40 -0400

    limit latest activity to 150 events for speed

--- app.rb
+++ app.rb
@@ -281,9 +281,9 @@ get '/packages?/?' do
                 names = ["submit", "reject", "ingest finished", "disseminate finished", "snafu", "disseminate snafu", "withdraw"]
 
                 if @is_op
-                  ps = Event.all(:timestamp => range, :name => names).packages
+                  ps = Event.all(:timestamp => range, :name => names, :limit => 150).packages
                 else
-                  ps = @user.account.projects.packages.events.all(:timestamp => range, :name => names).packages
+                  ps = @user.account.projects.packages.events.all(:timestamp => range, :name => names, :limit => 150).packages
                 end
               end
 
