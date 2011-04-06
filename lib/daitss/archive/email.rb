f3b787cfb0410781d067d748e441abf40123bbce
Manny Rodriguez <manny@bianchi.fcla.edu>
2011-04-06 18:13:47 -0400

    fix spacing for multipart emails

--- lib/daitss/archive/email.rb
+++ lib/daitss/archive/email.rb
@@ -18,18 +18,18 @@ Date: #{Time.now.to_s}
 Content-Type: text/plain; charset=us-ascii
 Content-Transfer-Encoding: 7bit
 Content-Type: multipart/alternative; boundary=#{marker}
---#{marker}
 
+--#{marker}
 Content-Type: text/plain
 Content-Transfer-Encoding: 7bit
 
 Ingest of package #{package.sip.name}
 --#{marker}
-
 Content-Type: multipart/mixed; name="#{package.id}.ingest.xml"
 Content-Transfer-Encoding: 7bit
 
 #{ingest_report package.id}
+
 --#{marker}--
 EOF
 
@@ -51,18 +51,18 @@ Date: #{Time.now.to_s}
 Content-Type: text/plain; charset=us-ascii
 Content-Transfer-Encoding: 7bit
 Content-Type: multipart/alternative; boundary=#{marker}
---#{marker}
 
+--#{marker}
 Content-Type: text/plain
 Content-Transfer-Encoding: 7bit
 
 #{package.events.first(:name => "reject").notes}
 --#{marker}
-
 Content-Type: multipart/mixed; name="#{package.id}.error.xml"
 Content-Transfer-Encoding: 7bit
 
 #{reject_report package.id}
+
 --#{marker}--
 EOF
 
