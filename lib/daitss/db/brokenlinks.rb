c4c96b413234a2795d21ad41c06a2c9f251d1a7d
Ira Terman <ira.terman1@gmail.com>
2012-10-26 15:09:10 -0400

    issue #19 improve,simplify code

--- lib/daitss/db/brokenlinks.rb
+++ lib/daitss/db/brokenlinks.rb
@@ -17,41 +17,17 @@ module Daitss
     belongs_to :datafile # the associated Datafile
 
     def fromPremis(df, premis)
-# pick off the first broken link only
-# premis looks like:
+            # <eventOutcomeDetailExtension>  looks like:
 	    #  <eventOutcomeDetailExtension>
 	    #   <broken_link type="stylesheet">http://schema.fcla.edu/xml/broken-stylesheet-student_html.xsl</broken_link>
-	    #   <broken_link type="unresolvable">http://www.w3.org/2001/XMLSchema</broken_link>
-	    #   <broken_link type="unresolvable">info:lc/xmlns/premis-v2-beta</broken_link>
 	    #  </eventOutcomeDetailExtension>
-      nodes = premis.find("premis:broken_link", NAMESPACES)
-      links = Array.new
-      nodes.each do |obj|
-        links << obj.content
-	break
-      end
-      attribute_set(:broken_links, links[0])
-
+      attribute_set(:broken_links, premis.content)
+      attribute_set(:type, premis['type'])
       df.broken_links << self
-      
-      kids  = Array.new
-      attrs_of_kids = Array.new
-      types = Array.new
-      values = Array.new
-      premis.children.each {|z| kids   << z}  # kids is a array of nodes
-      kids.each {|z| attrs_of_kids  << z.attributes}  # attrs_of_kids is an array of attributes
-
-      attrs_of_kids.each {|z| z.each {|w| types << w}}  # types will become an array each element holds a single attribute
-      types.each do 
-        |z| values << z.value
-        break
-      end
-      attribute_set(:type, values[0])
     end
 
     after :save do
       puts "#{self.errors.to_a} error encountered while saving #{self.inspect} " unless valid?
     end
   end
-
 end
