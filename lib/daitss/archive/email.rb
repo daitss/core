fc226083d0d211df5d8feadc124920757536749d
Ira Terman <ira.terman1@gmail.com>
2013-02-01 15:04:08 -0500

    #721,#719#688 click on ingest or disseminate produces html

--- lib/daitss/archive/email.rb
+++ lib/daitss/archive/email.rb
@@ -1,6 +1,7 @@
 require 'daitss/archive'
 require 'daitss/archive/report'
 require 'net/smtp'
+require 'nokogiri'
 
 module Daitss
 
@@ -9,9 +10,13 @@ module Daitss
     FROM = 'daitss@'+`hostname`.chomp
 
     def email_report package
-      reply_to = archive.mailer_reply_to      #'iterman@ufl.edu'
+      reply_to = archive.mailer_reply_to     
       account = package.project.account
 
+      xslt  = Nokogiri::XSLT(File.read('public/daitss_report_xhtml.xsl'))
+      doc = Nokogiri::XML ingest_report package.id 
+      transform = xslt.transform(doc)
+      html = transform.to_s
       marker = rand(1000000000000)
 
 
@@ -19,27 +24,29 @@ msg = <<EOF
 From: DAITSS <#{FROM}>
 Reply-To: #{reply_to}
 To: DAITSS Account #{account.id} <#{account.report_email}>
-Subject: Florida Digital Archive Report - Ingest of package #{package.sip.name}    ,IEID: #{package.id}
+Subject: Florida Digital Archive Report - Ingest of package #{package.sip.name}, IEID: #{package.id}
 Date: #{Time.now.to_s}
 Content-Type: multipart/mixed; boundary="#{marker}"
 
 --#{marker}
 Content-Type: text/plain
 
-Ingest of package #{package.sip.name}    ,IEID: #{package.id}
+Ingest of package #{package.sip.name}, IEID: #{package.id}
 --#{marker}
-Content-Type: text/xml; name="#{package.id}.ingest.xml"
+Content-Type: text/xml; name="#{package.id}_ingest_report.html"
 
-#{ingest_report package.id}
+#{html}                  
 
 --#{marker}--
+
+
 EOF
 
     email msg,FROM,account.report_email
     end
 
     def email_reject_report package
-      reply_to = archive.mailer_reply_to      #'iterman@ufl.edu'
+      reply_to = archive.mailer_reply_to    
       account = package.project.account
 
       marker = rand(1000000000000)
@@ -48,7 +55,7 @@ msg = <<EOF
 From: DAITSS <#{FROM}>
 Reply-To: #{reply_to}
 To: DAITSS Account #{account.id} <#{account.report_email}>
-Subject: Florida Digital Archive Report - Reject of package #{package.sip.name}    ,IEID: #{package.id}
+Subject: Florida Digital Archive Report - Reject of package #{package.sip.name}, IEID: #{package.id}
 Date: #{Time.now.to_s}
 Content-Type: multipart/mixed; boundary="#{marker}"
 
@@ -66,11 +73,14 @@ EOF
     email msg,FROM,account.report_email
     end
 
-  #-->
 
     def email_dissemination_report package
-      reply_to = archive.mailer_reply_to      #'iterman@ufl.edu'
+      reply_to = archive.mailer_reply_to     
       account = package.project.account
+      xslt  = Nokogiri::XSLT(File.read('public/daitss_disseminate_report_xhtml.xsl'))
+      doc = Nokogiri::XML disseminate_report package.id 
+      transform = xslt.transform(doc)
+      html = transform.to_s
 
       marker = rand(1000000000000)
 
@@ -78,19 +88,20 @@ msg = <<EOF
 From: DAITSS <#{FROM}>
 Reply-To: #{reply_to}
 To: DAITSS Account #{account.id} <#{account.report_email}>
-Subject: Florida Digital Archive Report - Dissemination of package #{package.sip.name}    ,IEID: #{package.id}
+Subject: Florida Digital Archive Report - Dissemination of package #{package.sip.name}, IEID: #{package.id}
 Date: #{Time.now.to_s}
 Content-Type: multipart/mixed; boundary="#{marker}"
 
 --#{marker}
 Content-Type: text/plain
 
-Dissemination of package #{package.sip.name}    ,IEID:#{package.id}
+Dissemination of package #{package.sip.name}, IEID: #{package.id}
 #{package.events.last(:name => "disseminate request placed").notes}
+ 
 --#{marker}
-Content-Type: text/xml; name="#{package.id}.disseminate.xml"
+Content-Type: text/xml; name="#{package.id}_disseminate_report.html"
 
-#{disseminate_report package.id}
+#{html}                  
 
 --#{marker}--
 EOF
@@ -106,9 +117,8 @@ EOF
         smtp.send_message msg, from, to
      end
 	  rescue
-		  puts "email problem rc=#{$!}"
+		  puts "email problem rc=#{$!} \nsmtp_host=#{smtp_host},from=#{from},to=#{to}"
 	  end
   end
-  #<--
 end
  
