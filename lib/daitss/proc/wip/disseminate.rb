a2e6224cd221b8ac99d05dc27135d909cc4f3232
Ira Terman <ira.terman1@gmail.com>
2013-02-11 15:07:55 -0500

    #699 dissemination directory now includes as it last node account_id

--- lib/daitss/proc/wip/disseminate.rb
+++ lib/daitss/proc/wip/disseminate.rb
@@ -45,7 +45,12 @@ module Daitss
     end
 
     def next_drop_path
-      pattern = File.join archive.disseminate_path, "#{id}-*.tar"
+      dirname = File.join archive.disseminate_path, package.project_account_id	    
+      if ! File.exist?(dirname)
+	      Dir.mkdir(dirname)
+      end
+
+      pattern = File.join dirname,   "#{id}-*.tar"  #github 700
       dips = Dir[pattern]
 
       n = if dips.empty?
@@ -54,7 +59,7 @@ module Daitss
             dips.map { |f| File.basename(f)[%r{#{id}-(\d+).tar}, 1].to_i }.max + 1
           end
 
-      File.join archive.disseminate_path, "#{id}-#{n}.tar"
+      File.join dirname, "#{id}-#{n}.tar"  #github #700
     end
 
   end
