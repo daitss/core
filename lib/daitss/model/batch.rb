6f216ace719d86345669946efa8aabd940433325
Jon <jpitts@flvc.org>
2014-09-29 14:09:21 -0400

    #701 Batch model enhancement for finding number of submitted files and size.

--- lib/daitss/model/batch.rb
+++ lib/daitss/model/batch.rb
@@ -11,6 +11,23 @@ module Daitss
 
     has n, :batch_assignments
     has n, :packages, :through => :batch_assignments
+    
+    def size_in_bytes
+      size = 0
+      self.packages.each do |p|
+        size += p.sip.size_in_bytes
+      end
+      size
+    end
+    
+    def submitted_datafiles
+      num = 0
+      self.packages.each do |p|
+        num += p.sip.submitted_datafiles
+      end
+      num
+    end
+    
   end
 
   class BatchAssignment
